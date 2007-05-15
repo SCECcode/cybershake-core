df3ac687cf50eab2551e4e3edde375f54643be0b
Scott Callaghan <scottcal@usc.edu>
2007-05-15 00:15:57 +0000

    SC:  added test DB connect

---
+++ db/TestDBConnect.java
@@ -0,0 +1,22 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+public class TestDBConnect {
+
+	public static void main(String[] args) {
+		DBConnect dbc = new DBConnect("intensity.usc.edu","CyberShake");
+		ResultSet rs = dbc.selectData("SHOW TABLES");
+		try {
+			System.out.println(rs.getMetaData().getColumnCount());
+			rs.first();
+			while (!rs.isAfterLast()) {
+				System.out.println(rs.getString(1));
+				rs.next();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
