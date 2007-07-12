2312e610af4ef89a9e1164e88a927de98c8cb8de
Joshua Garcia <joshuaga@usc.edu>
2007-07-12 11:28:04 +0000

    - created SQL Query test

---
+++ CyberCommands/test/databaseconnection/SQLQueryForSiteID.java
@@ -0,0 +1,32 @@
+package test.databaseconnection;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+import junit.framework.JUnit4TestAdapter;
+
+public class SQLQueryForSiteID {
+	
+	@Test public void sqlQueryForSiteID() {
+		SessionFactory sessFactory = new Configuration().configure("intensity.cfg.xml").buildSessionFactory();
+		Session retrieveSiteIDSess = sessFactory.openSession();
+		
+		String siteIDQuery = "SELECT CS_Site_ID FROM CyberShake_Sites WHERE CS_Site_Name = USC";
+		//System.out.println(query);
+		List siteIDList = retrieveSiteIDSess.createSQLQuery(siteIDQuery).addScalar("CS_Site_ID", Hibernate.INTEGER).list();
+		Object siteIDObject = siteIDList.get(0); 
+		int siteID = (Integer)siteIDObject;
+		
+		assertEquals(18,siteID);
+	}
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter (SQLQueryForSiteID.class);
+	}
+}
