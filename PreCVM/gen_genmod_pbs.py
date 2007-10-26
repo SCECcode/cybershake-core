c10511f21c3eb887175ef3bec3babcf92d77c1f5
Scott Callaghan <scottcal@usc.edu>
2007-10-26 22:21:23 +0000

    SC:  creates genmod script for CVM PBS runs

--- PreCVM/gen_genmod_pbs.py
+++ PreCVM/gen_genmod_pbs.py
@@ -1,124 +1,95 @@
 #!/usr/bin/python
 
-def genDepFile("zdepths.meter", gridout):
+import sys
+
+def genDepFile(zdepth, gridout):
+    '''this replicates the gawk code from RWG in genmod.XXX.pbs -- it writes a list of all depths from the gridout file to the zdepth file.  It returns the number of steps in the X, Y, and Z directions as a tuple.'''
     KM2METER = 1000.0
-    input = open("../../data/ModelParams/gridout_" + site)
-    iz = 0
-    go = 0
-    for line in input:
-        split...
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    BEGIN{iz=0;go=0;} {
-    split($1,a,"=");
-    if(a[1]=="nz") {
-        nz=a[2];
-        go=1;
-    } else if(go==1 && iz<nz) {
-        iz++;
-        dep[iz]=$2*cnv;
-        if(dep[iz]<0.0)
-            dep[iz]=0.0;
-    }
-} END {
-    for(iz=1;iz<=nz;iz++) {
-        printf "%14.2f\n",dep[iz];
-    }
-}
+    input = open(gridout)
+    output = open(zdepth, "w")
+    inputContents = input.readlines()
+    intNX = int((inputContents[1].split("="))[1])
+    intNY = int((inputContents[1+intNX+2].split("="))[1])
+    intNZ = int((inputContents[1+intNX+2+intNY+2].split("="))[1])
+    for i in range(len(inputContents)-intNZ, len(inputContents)):
+        print float(inputContents[i].split()[1])
+        output.write("%14.2f\n" % (float(inputContents[i].split()[1])*KM2METER))
+    output.flush()
+    output.close()
+    return [intNX, intNY, intNZ]
 
 
 if len(sys.argv) < 2:
     print "Usage:  gen_genmod_pbs.py <site>"
     print "Example: gen_genmod_pbs.py USC"
-    exit(1)
+    sys.exit(1)
 
 site = sys.argv[1]
-output = open("../V4-WrapC/genmod." + site + ".pbs", "w")
-output.writeLine('#!/bin/csh')
-output.writeLine('#*** The "#PBS" lines must come before any non-blank non-comment lines ***')
-output.writeLine('#PBS -e genmod.' + site + '.e -o genmod.' + site + '.o')
-output.writeLine('#PBS -l walltime=05:00:00,nodes=50:myri:ppn=2')
-output.writeLine('\nif ($?PBS_JOBID) then           # if this is a PBS job')
-output.writeLine('  echo "Starting" $PBS_JOBID `date`')
-output.writeLine('  echo "Initiated on `hostname`"')
-output.writeLine('  echo ""')
-output.writeLine('  cd "$PBS_O_WORKDIR"           # connect to working directory of qsub')
-output.writeLine('else')
-output.writeLine('  echo "This script must be run as a PBS job"')
-output.writeLine('  exit 1')
-output.writeLine('endif')
-output.writeLine('\nif ($?PBS_NODEFILE) then')
-output.writeLine('  #count the number of processors assigned by PBS')
-output.writeLilne('  set NP = `wc -l < $PBS_NODEFILE `')
-output.writeLine('  echo "Running on $NP processors: "`cat $PBS_NODEFILE`')
-output.writeLine('else')
-output.writeLine('  echo "This job did not have the number of nodes specified with"')
-output.writeLine('  echo "the node= resource"')
-output.writeLine('  exit 1')
-output.writeLine('endif')
-output.writeLine('\n#The following should contain your program and any arguments')
-output.writeLine('set BPATH = `pwd`')
-output.writeLine('set BPROG = bin/ver4C-mpi')
-output.writeLine('set MODELDIR = ${BPATH}/DataFiles')
-output.writeLine('\nset NX = 
-output.writeLine('set NY = 
-output.writeLine('set NZ = 
-output.writeLine('\nset OUTDIR = ../../data/LayerBin')
-output.writeLine('set SCRATCH_OUTDIR = /scratch/pbsjob-${PBS_JOBID}/data/LayerBin')
-output.writeLine('set MAIN_RUNDIR = ./')
-output.writeLine('\mkdir -p ${SCRATCH_OUTDIR}')
-output.writeLine('ls -lt $SCRATCH_OUTDIR')
-output.writeLine('\nset FILEROOT = v4_sgt')
-output.writeLine('set LOGDIR = ../../logs/GenLog')
-output.writeLine('\nset MODELCORDS = ../../data/ModelParams/' + site + '/model_coords_GC_' + site)
-output.writeLine('set DEPFILE = zdepths.meter')
 
+zdepthFile = "../../data/ModelParams/" + site + "/zdepths.meter"
 gridout = '../../data/ModelParams/' + site + '/gridout_' + site
-genDepFile("zdepths.meter", gridout)
+numSteps = genDepFile(zdepthFile, gridout)
 
-gawk -v cnv=$KM2METER 'BEGIN{iz=0;go=0;}{ \
-split($1,a,"="); \
-if(a[1]=="nz"){nz=a[2];go=1;} \
-else if(go==1 && iz<nz){iz++;dep[iz]=$2*cnv; if(dep[iz]<0.0)dep[iz]=0.0;}} \
-END { \
-for(iz=1;iz<=nz;iz++){printf "%14.2f\n",dep[iz];}}' \
-$GRIDOUT > $DEPFILE
+output = open("../V4-WrapC/genmod." + site + ".pbs", "w")
+output.write('#!/bin/csh\n')
+output.write('#*** The "#PBS" lines must come before any non-blank non-comment lines ***\n')
+output.write('#PBS -e genmod.' + site + '.e -o genmod.' + site + '.o\n')
+output.write('#PBS -l walltime=05:00:00,nodes=50:myri:ppn=2\n')
 
-output.writeLine('mpiexec ${BPATH}/${BPROG} \ ')
-output.writeLine('         nx=$NX ny=$NY nz=$NZ \ ')
-output.writeLine('         cordfile=$MODELCORDS \ ')
-output.writeLine('         depfile=$DEPFILE \ ')
-output.writeLine('         modeldir=$MODELDIR \ ')
-output.writeLine('         outdir=$SCRATCH_OUTDIR \ ')
-output.writeLine('         fileroot=$FILEROOT \ ')
-output.writeLine('         logdir=$LOGDIR')
-output.writeLine('\n\cp -r $SCRATCH_OUTDIR $OUTDIR')
-output.writeLine('\necho "Done   " `date`')
-output.writeLine('\nexit')
+output.write('\nif ($?PBS_JOBID) then           # if this is a PBS job\n')
+output.write('  echo "Starting" $PBS_JOBID `date`\n')
+output.write('  echo "Initiated on `hostname`"\n')
+output.write('  echo ""\n')
+output.write('  cd "$PBS_O_WORKDIR"           # connect to working directory of qsub\n')
+output.write('else\n')
+output.write('  echo "This script must be run as a PBS job"\n')
+output.write('  exit 1\n')
+output.write('endif\n')
 
+output.write('\nif ($?PBS_NODEFILE) then\n')
+output.write('  #count the number of processors assigned by PBS\n')
+output.write('  set NP = `wc -l < $PBS_NODEFILE `\n')
+output.write('  echo "Running on $NP processors: "`cat $PBS_NODEFILE`\n')
+output.write('else\n')
+output.write('  echo "This job did not have the number of nodes specified with"\n')
+output.write('  echo "the node= resource"\n')
+output.write('  exit 1\n')
+output.write('endif\n')
 
+output.write('\n#The following should contain your program and any arguments\n')
+output.write('set BPATH = `pwd`\n')
+output.write('set BPROG = bin/ver4C-mpi\n')
+output.write('set MODELDIR = ${BPATH}/DataFiles\n')
 
+output.write('\nset NX = %d\n' % numSteps[0])
+output.write('set NY = %d\n' % numSteps[1])
+output.write('set NZ = %d\n' % numSteps[2])
 
-BEGIN{iz=0;go=0;} {
-    split($1,a,"=");
-    if(a[1]=="nz") {
-        nz=a[2];
-        go=1;
-    } else if(go==1 && iz<nz) {
-        iz++;
-        dep[iz]=$2*cnv;
-        if(dep[iz]<0.0)
-            dep[iz]=0.0;
-    }
-} END {
-    for(iz=1;iz<=nz;iz++) {
-        printf "%14.2f\n",dep[iz];
-    }
-}
+output.write('\nset OUTDIR = ../../data/LayerBin\n')
+output.write('set SCRATCH_OUTDIR = /scratch/pbsjob-${PBS_JOBID}/data/LayerBin\n')
+output.write('set MAIN_RUNDIR = ./\n')
+output.write('\mkdir -p ${SCRATCH_OUTDIR}\n')
+output.write('ls -lt $SCRATCH_OUTDIR\n')
+
+output.write('\nset FILEROOT = v4_sgt\n')
+output.write('set LOGDIR = ../../logs/GenLog\n')
+
+output.write('\nset MODELCORDS = ../../data/ModelParams/' + site + '/model_coords_GC_' + site + '\n')
+output.write('set DEPFILE = ' + zdepthFile + '\n')
+output.write('mpiexec ${BPATH}/${BPROG} \ \n')
+output.write('         nx=$NX ny=$NY nz=$NZ \ \n')
+output.write('         cordfile=$MODELCORDS \ \n')
+output.write('         depfile=$DEPFILE \ \n')
+output.write('         modeldir=$MODELDIR \ \n')
+output.write('         outdir=$SCRATCH_OUTDIR \ \n')
+output.write('         fileroot=$FILEROOT \ \n')
+output.write('         logdir=$LOGDIR\n')
+
+output.write('\n\cp -r $SCRATCH_OUTDIR/* $OUTDIR\n')
+
+output.write('\necho "Done   " `date`\n')
+
+output.write('\nexit\n')
+
+output.flush()
+output.close()
