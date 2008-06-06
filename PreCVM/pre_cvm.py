c2da88275153295db922d0a760974af512cda0e1
Scott Callaghan <scottcal@usc.edu>
2008-06-06 18:03:59 +0000

    updated reporting of exitcodes

--- PreCVM/pre_cvm.py
+++ PreCVM/pre_cvm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/u/ac/scottcal/python-2.5.1/bin/python
 
 import sys;
 import os;
@@ -6,7 +6,7 @@ import os;
 if len(sys.argv) < 8:
     print "Usage: pre_cvm.py <site> <modelboxFile> <gridfile> <gridout> <coordfile> <paramsfile> <boundsfile>"
     print "Example: pre_cvm.py USC USC.modelbox gridfile_USC gridout_USC model_coords_GC_USC model_params_GC_USC model_bounds_GC_USC"
-    sys.exit()
+    sys.exit(-1)
 
 site = sys.argv[1]
 modelbox = os.path.abspath(sys.argv[2])
@@ -16,7 +16,10 @@ coordsfile = os.path.abspath(sys.argv[5])
 paramsfile = os.path.abspath(sys.argv[6])
 boundsfile = os.path.abspath(sys.argv[7])
 
-os.chdir("Modelbox")
-os.system("./get_modelbox.py %s %s" % (site, modelbox))
+os.chdir(os.path.join(sys.path[0], "Modelbox"))
+exitcode = os.system("./get_modelbox.py %s %s" % (site, modelbox))
+if exitcode!=0:
+	sys.exit((exitcode >> 8) & 0xFF)
 os.chdir("../GenGrid_py")
-os.system("./gen_grid.py %s %s %s %s %s %s" % (modelbox, gridfile, gridout, coordsfile, paramsfile, boundsfile))
+exitcode = os.system("./gen_grid.py %s %s %s %s %s %s" % (modelbox, gridfile, gridout, coordsfile, paramsfile, boundsfile))
+sys.exit((exitcode >> 8) & 0xFF)
