26823e93a79f6f4decbf6f626f6433bfea17a18a
Scott Callaghan <scottcal@usc.edu>
2009-04-28 17:29:05 +0000

    SC:  modifications to work with Run IDs

--- CyberCommands/mapping/PeakAmplitudePK.java
+++ CyberCommands/mapping/PeakAmplitudePK.java
@@ -7,21 +7,12 @@ public class PeakAmplitudePK implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private int ERF_ID;
-	private int Site_ID; 
 	private int Source_ID;
 	private int Rupture_ID;
 	private long Rup_Var_ID;
-	private int Rup_Var_Scenario_ID;
-	private int SGT_Variation_ID;
+	private int Run_ID;
 	private int IM_Type_ID;
-	
-	public int getERF_ID() {
-		return ERF_ID;
-	}
-	public void setERF_ID(int erf_id) {
-		ERF_ID = erf_id;
-	}
+
 	public int getIM_Type_ID() {
 		return IM_Type_ID;
 	}
@@ -34,19 +25,6 @@ public class PeakAmplitudePK implements Serializable {
 	public void setRup_Var_ID(long rup_Var_ID) {
 		Rup_Var_ID = rup_Var_ID;
 	}
-	public int getRup_Var_Scenario_ID() {
-		return Rup_Var_Scenario_ID;
-	}
-	public void setRup_Var_Scenario_ID(int rup_Var_Scenario_ID) {
-		Rup_Var_Scenario_ID = rup_Var_Scenario_ID;	
-	}
-	
-	public int getSGT_Variation_ID() {
-		return SGT_Variation_ID;
-	}
-	public void setSGT_Variation_ID(int variation_ID) {
-		SGT_Variation_ID = variation_ID;
-	}
 	
 	public int getRupture_ID() {
 		return Rupture_ID;
@@ -54,17 +32,18 @@ public class PeakAmplitudePK implements Serializable {
 	public void setRupture_ID(int rupture_ID) {
 		Rupture_ID = rupture_ID;
 	}
-	public int getSite_ID() {
-		return Site_ID;
-	}
-	public void setSite_ID(int site_ID) {
-		Site_ID = site_ID;
-	}
 	public int getSource_ID() {
 		return Source_ID;
 	}
 	public void setSource_ID(int source_ID) {
 		Source_ID = source_ID;
+	}
+	
+	public int getRun_ID() {
+		return Run_ID;
+	}
+	public void setRun_ID(int run_ID) {
+		Run_ID = run_ID;
 	}
 	
 	public boolean equals(Object obj)
@@ -75,13 +54,10 @@ public class PeakAmplitudePK implements Serializable {
 			return false;
 		// object must be PeakAmplitudesPK at this point
 		PeakAmplitudePK papk = (PeakAmplitudePK)obj;
-		return (ERF_ID == papk.getERF_ID() &&
-				Site_ID == papk.getSite_ID() &&
-				Source_ID == papk.getSource_ID() &&
+		return (Source_ID == papk.getSource_ID() &&
 				Rupture_ID == papk.getRupture_ID() && 
 				Rup_Var_ID == papk.getRup_Var_ID() &&
-				Rup_Var_Scenario_ID == papk.getRup_Var_Scenario_ID() &&
-				SGT_Variation_ID == papk.getSGT_Variation_ID() &&
+				Run_ID == papk.getRun_ID() &&
 				IM_Type_ID == papk.getIM_Type_ID() );
 	}
 	
@@ -90,8 +66,8 @@ public class PeakAmplitudePK implements Serializable {
 	}
 	
 	public String toString() {
-		return "ERF_ID: " + ERF_ID + ", Site_ID: " + Site_ID + ", Source_ID: " + Source_ID + ", Rupture_ID: " + Rupture_ID
-		+ ", Rup_Var_ID: " + Rup_Var_ID + ", Rup_Var_Scenario_ID: " + Rup_Var_Scenario_ID + ", SGT_Variation_ID: " + SGT_Variation_ID + ", IM_Type_ID: " + IM_Type_ID;
+		return "Run_ID: " + Run_ID + ", Source_ID: " + Source_ID + ", Rupture_ID: " + Rupture_ID
+		+ ", Rup_Var_ID: " + Rup_Var_ID + ", IM_Type_ID: " + IM_Type_ID;
 	}
 
 }
