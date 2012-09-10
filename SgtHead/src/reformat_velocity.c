773b3b73153ad7e7a665362ea778c6c43d709962
Scott Callaghan <scottcal@usc.edu>
2012-09-10 16:15:18 +0000

    Modified reformat velocity to work with files too big to fit in mem

--- SgtHead/src/reformat_velocity.c
+++ SgtHead/src/reformat_velocity.c
@@ -24,37 +24,66 @@ int main(int argc, char** argv) {
 	FILE* d_fp = fopen(d_name, "rb");
 	FILE* out_fp = fopen(argv[5], "wb");
 
-	int i, j, k, in_offset, out_offset;
-	float* p_data, *s_data, *d_data, *out_data;
+	int i, j, k, out_offset;
+	float* p_data, *s_data, *d_data, *awp_buffer;
 
-	p_data = malloc(nx*ny*nz*sizeof(float));
-        s_data = malloc(nx*ny*nz*sizeof(float));
-        d_data = malloc(nx*ny*nz*sizeof(float));
-	out_data = malloc(3*nx*ny*nz*sizeof(float));
 
-	fread(p_data, sizeof(float), nx*ny*nz, p_fp);
-        fread(s_data, sizeof(float), nx*ny*nz, s_fp);
-        fread(d_data, sizeof(float), nx*ny*nz, d_fp);
+	//RWG is fast x, z, y (km)
+	//AWP is fast y, x, z (actually fast x, y, z, but y and x are flipped)
+
+	int chunk_size = (int)(175000000.0/(nx*nz));
+	if (chunk_size>ny) {
+		chunk_size = ny;
+	}
+
+	printf("Using chunk size %d.\n", chunk_size);	
+	p_data = malloc(nx*chunk_size*nz*sizeof(float));
+	s_data = malloc(nx*chunk_size*nz*sizeof(float));	
+	d_data = malloc(nx*chunk_size*nz*sizeof(float));
+
+	awp_buffer = malloc(3*chunk_size*sizeof(float));
+
+	int y_index = 0;
+	int in_arr_offset, max_y;
+	long outfile_offset;
+	while (y_index<ny) {
+		max_y = chunk_size;
+		if (y_index+chunk_size>ny) {
+			max_y = ny-y_index;
+		}
+
+		fread(p_data, sizeof(float), nx*max_y*nz, p_fp);
+		fread(s_data, sizeof(float), nx*max_y*nz, s_fp);
+		fread(d_data, sizeof(float), nx*max_y*nz, d_fp);
+
+		for(k=0; k<nz; k++) {
+			//printf("%d-%d:  Z slice %d of %d.\n", y_index, y_index+max_y, k+1, nz);
+			//fflush(stdout);
+			for (i=0; i<nx; i++) {
+				for (j=0; j<max_y; j++) {
+					in_arr_offset = j*nx*nz + k*nx + i;
+					//printf("In offset: %d\n", in_arr_offset);
+					awp_buffer[3*j] = p_data[in_arr_offset]*1000.0;
+					awp_buffer[3*j+1] = s_data[in_arr_offset]*1000.0;
+					awp_buffer[3*j+2] = d_data[in_arr_offset]*1000.0;
+				}
+				outfile_offset = 3*sizeof(float)*(k*nx*ny + i*ny + y_index);
+				fseek(out_fp, outfile_offset, SEEK_SET);
+				fwrite(awp_buffer, sizeof(float), 3*max_y, out_fp);
+			}
+		}
+		y_index += max_y;
+	}
 
 	fclose(p_fp);
 	fclose(s_fp);
 	fclose(d_fp);
-
-	for(i=0; i<nz; i++) {
-		printf("%d of %d z-slices.\n", i, nz);
-		for (j=0; j<nx; j++) {
-			for (k=0; k<ny; k++) {
-				in_offset = k*nx*nz + i*nx + j;
-				out_offset = 3*(i*nx*ny + j*ny + k);
-				out_data[out_offset] = p_data[in_offset]*1000.0;
-				out_data[out_offset+1] = s_data[in_offset]*1000.0;
-				out_data[out_offset+2] = d_data[in_offset]*1000.0;
-			}
-		}
-	}
-
-	fwrite(out_data, sizeof(float), 3*nx*ny*nz, out_fp);
 	fflush(out_fp);
 	fclose(out_fp);
+	
+	free(p_data);
+	free(s_data);
+	free(d_data);
+	free(awp_buffer);
 	return 0;
 }
