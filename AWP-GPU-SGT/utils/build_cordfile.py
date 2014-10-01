665b15dc9ab747db80c67a7df55cc08a0b5f3264
Scott Callaghan <scottcal@usc.edu>
2014-10-01 19:35:18 +0000

    Added calculation of max depth index as argument for cordfile creation

--- AWP-GPU-SGT/utils/build_cordfile.py
+++ AWP-GPU-SGT/utils/build_cordfile.py
@@ -4,10 +4,10 @@ import sys
 import os
 import config
 
-def build_cordfile(site, rwg_cordfile, awp_cordfile):
+def build_cordfile(site, rwg_cordfile, awp_cordfile, max_depth):
 	cs_path = config.getProperty("CS_PATH")
 	
-	cmd = "%s/SgtHead/gen_awp_cordfile.py %s %s" % (cs_path, rwg_cordfile, awp_cordfile)
+	cmd = "%s/SgtHead/gen_awp_cordfile.py %s %s" % (cs_path, rwg_cordfile, awp_cordfile, max_depth)
 
 	exitcode = os.system(cmd)
 	return ((exitcode >> 8) & 0xFF)
