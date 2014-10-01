15fd10a3a3f55275bbd57706842d1c042bdb423c
Scott Callaghan <scottcal@usc.edu>
2014-10-01 20:06:56 +0000

    Fixed omitted argument

--- AWP-GPU-SGT/utils/build_cordfile.py
+++ AWP-GPU-SGT/utils/build_cordfile.py
@@ -7,7 +7,7 @@ import config
 def build_cordfile(site, rwg_cordfile, awp_cordfile, max_depth):
 	cs_path = config.getProperty("CS_PATH")
 	
-	cmd = "%s/SgtHead/gen_awp_cordfile.py %s %s" % (cs_path, rwg_cordfile, awp_cordfile, max_depth)
+	cmd = "%s/SgtHead/gen_awp_cordfile.py %s %s %d" % (cs_path, rwg_cordfile, awp_cordfile, max_depth)
 
 	exitcode = os.system(cmd)
 	return ((exitcode >> 8) & 0xFF)
