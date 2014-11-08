72c79b26501f1d643d601b68eb63c50e093c9d2a
Scott Callaghan <scottcal@usc.edu>
2014-11-08 01:43:49 +0000

    Added memsets for when there are fewer args than specified in max args

--- RuptureCodes/RupGen-api-3.3.1/src/rupgen_api.c
+++ RuptureCodes/RupGen-api-3.3.1/src/rupgen_api.c
@@ -35,6 +35,7 @@ int rupgen_get_num_rv(char* rup_geom_file, rg_stats_t *stats, int hypo_dist) {
 	rgargv = malloc(rgargc * sizeof(char*));
 	for (j = 0; j < NUM_GENSLIP_ARGS; j++) {
 		rgargv[j] = malloc(MAX_STR_ARGV);
+		memset(rgargv[j], 0, MAX_STR_ARGV);
 	}
 	sprintf(rgargv[0], "%s", "rupgen");
 	sprintf(rgargv[1], "infile=%s", rup_geom_file);
@@ -89,6 +90,7 @@ int rupgen_genslip(char* rup_geom_file, int slip, int hypo, rg_stats_t *stats, s
         rgargv = malloc(rgargc * sizeof(char*));
         for (j = 0; j < NUM_GENSLIP_ARGS; j++) {
                 rgargv[j] = malloc(MAX_STR_ARGV);
+		memset(rgargv[j], 0, MAX_STR_ARGV);
         }
         sprintf(rgargv[0], "%s", "rupgen");
         sprintf(rgargv[1], "infile=%s", rup_geom_file);
