9a89885128fa2522a7b70db1f89e06585288084f
Scott Callaghan <scottcal@usc.edu>
2015-03-10 21:53:46 +0000

    Pulled out convolution into accumulator

--- DirectSynth/src/jbsim/stf_subs.c
+++ DirectSynth/src/jbsim/stf_subs.c
@@ -15,19 +15,31 @@ uv = u;
 un = u + nt;
 ue = u + 2*nt;
 
+float acc[3];
 for(it=0;it<nt;it++)
    {
    kend = it + 1;
    if(kend > nstf)
       kend = nstf;
 
-   for(k=0;k<kend;k++)
+   acc[0] = acc[1] = acc[2] = 0.0;
+   for(k=0;k<kend;k++) {
+	acc[0] += stf[k]*uv[it-k];
+	acc[1] += stf[k]*un[it-k];
+	acc[2] += stf[k]*ue[it-k];
+   }
+   sv[it] += acc[0];
+   sn[it] += acc[1];
+   se[it] += acc[2];
+   }
+
+   /*for(k=0;k<kend;k++)
       {
       sv[it] = sv[it] + stf[k]*uv[it-k];
       sn[it] = sn[it] + stf[k]*un[it-k];
       se[it] = se[it] + stf[k]*ue[it-k];
       }
-   }
+   }*/
 }
 
 void sum_nostf(float *s,float *u,float *slip,int nt)
