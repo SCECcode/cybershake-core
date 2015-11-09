eea4bdd3d4f2485fa9a2e99a4af9b40089bb0a41
Scott Callaghan <scottcal@usc.edu>
2015-11-09 20:47:37 +0000

    Added an option for different stream type

--- CyberCommands/data/BSAHeader.java
+++ CyberCommands/data/BSAHeader.java
@@ -36,6 +36,81 @@ public class BSAHeader {
 	
 	public BSAHeader() {}
 	
+	public int parse(ByteArrayInputStream bStream) throws IOException {
+		num_comps = 0;
+		x_comp = false;
+		y_comp = false;	
+		z_comp = false;
+		int i, ret;
+		byte[] byteArray = new byte[version.length];
+		ret = bStream.read(byteArray);
+		if (ret==-1) { //EOF 
+			return -1;
+		}
+			
+		StringBuffer sb = new StringBuffer();
+		//This will stop short if we find a null terminator
+		for (i=0; i<version.length; i++) {
+			version[i] = (char) byteArray[i];
+			if (version[i]!='\0') {
+				sb.append(version[i]);
+			} else {
+				break;
+			}
+		}
+		versionString = sb.toString();
+		if (!versionString.equals("12.10")) {
+			System.err.println("Version of this header is " + versionString + ", don't know how to parse it.");
+			System.exit(-5);
+		}
+		
+		byteArray = new byte[site_name.length];
+		bStream.read(byteArray);
+		sb = new StringBuffer();
+		for (i=0; i<site_name.length; i++) {
+			site_name[i] = (char) byteArray[i];
+			if (site_name[i]!='\0') {
+				sb.append(site_name[i]);
+			} else {
+				break;
+			}
+		}
+		siteString = sb.toString();
+		
+		bStream.skip((long)(padding.length));
+		//read the remaining bytes into an array, swap them, then read them out with a stream
+		byteArray = new byte[32];
+		bStream.read(byteArray);
+		byte[] outByteArray = SwapBytes.swapByteArrayToByteArrayForFloats(byteArray);
+		
+		DataInputStream dr = new DataInputStream(new ByteArrayInputStream(outByteArray));
+		source_id = dr.readInt();
+		rupture_id = dr.readInt();
+		rup_var_id = dr.readInt();
+		dt = dr.readFloat();
+		nt = dr.readInt();
+		comps = dr.readInt();
+		det_max_freq = dr.readFloat();
+		stoch_max_freq = dr.readFloat();
+		
+		if ((comps & X_COMP_FLAG)!=0) {
+			x_comp = true;
+			num_comps++;
+		}
+		if ((comps & Y_COMP_FLAG)!=0) {
+			y_comp = true;
+			num_comps++;
+		}
+		if ((comps & Z_COMP_FLAG)!=0) {
+			z_comp = true;
+			num_comps++;
+		}
+		
+		dr.close();
+		
+		return 0;
+	}
+	
 	public int parse(FileInputStream stream) throws IOException {
 		num_comps = 0;
 		x_comp = false;
