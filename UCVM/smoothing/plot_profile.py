609889a22cc4c5cc2cb480a0fb6dae5de98a5cf3
Scott Callaghan <scottcal@usc.edu>
2016-02-12 20:39:11 +0000

    Initial commit of smoothing contents

---
+++ UCVM/smoothing/plot_profile.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import struct
+import matplotlib
+matplotlib.use("AGG", warn=False)
+from pylab import *
+
+if len(sys.argv)<10:
+	print "Usage: %s <velocity file 1> <model coords file> <nx> <ny> <nz> <x-coord 1> <y-coord 1> <output file>" % sys.argv[0]
+	sys.exit(1)
+
+velocity_file_1 = sys.argv[1]
+model_coords_file = sys.argv[2]
+nx = int(sys.argv[3])
+ny = int(sys.argv[4])
+nz = int(sys.argv[5])
+x_coord_1 = int(sys.argv[6])
+y_coord_1 = int(sys.argv[7])
+x_coord_2 = int(sys.argv[8])
+y_coord_2 = int(sys.argv[9])
+output_filename = sys.argv[10]
+
+'''
+with open(model_coords_file, "r") as fp_in:
+        for line in fp_in:
+                pieces = line.split()
+		if int(pieces[2])==x_coord and int(pieces[3])==y_coord:
+			target_lon = float(pieces[0])
+			target_lat = float(pieces[1])
+'''
+print "Reading velocity file 1."
+
+profile_1 = []
+profile_2 = []
+for i in range(0, 3):
+	profile_1.append([])
+	profile_2.append([])
+
+with open(velocity_file_1, "rb") as fp_in:
+	for z in range(0, nz):
+		offset = 4*3*(z*nx*ny + x_coord_1*ny + y_coord_1)
+		fp_in.seek(offset, os.SEEK_SET)
+		data_str = fp_in.read(12)
+		data = struct.unpack("3f", data_str)
+		for i in range(0, 3):
+			profile_1[i].append(data[i])
+		offset = 4*3*(z*nx*ny + x_coord_2*ny + y_coord_2)
+                fp_in.seek(offset, os.SEEK_SET)
+                data_str = fp_in.read(12)
+                data = struct.unpack("3f", data_str)
+                for i in range(0, 3):
+                        profile_2[i].append(data[i])
+
+'''
+print "Reading velocity file 2."
+profile_2 = []
+for i in range(0, 3):
+        profile_2.append([])
+
+with open(velocity_file_2, "rb") as fp_in:
+        for z in range(0, nz):
+                offset = 4*3*(z*nx*ny + x_coord*ny + y_coord)
+                fp_in.seek(offset, os.SEEK_SET)
+                data_str = fp_in.read(12)
+                data = struct.unpack("3f", data_str)
+                for i in range(0, 3):
+                        profile_2[i].append(data[i])
+'''
+
+depths = []
+for i in range(0, nz):
+	depths.append(-0.4*i)
+
+
+clf()
+subplots_adjust(left=0.05)
+subplots_adjust(right=0.95)
+subplots_adjust(top=0.92)
+subplots_adjust(bottom=0.08)
+subplot(131, title="Vp")
+plot(profile_1[0], depths, label="x=%d, y=%d" % (x_coord_1, y_coord_1))
+plot(profile_2[0], depths, label="x=%d, y=%d" % (x_coord_2, y_coord_2))
+xlabel("Vp (km/s)")
+ylabel("Depth (km)")
+xlim(0, 8000)
+legend(loc="lower left")
+subplot(132, title="Vs")
+plot(profile_1[1], depths, label="x=%d, y=%d" % (x_coord_1, y_coord_1), marker='s')
+plot(profile_2[1], depths, label="x=%d, y=%d" % (x_coord_2, y_coord_2), marker='s')
+xlabel("Vs (km/s)")
+#xlim(0, 5000)
+xlim(3000, 5000)
+ylim(-35, -25)
+ylabel("Depth (km)")
+grid(b=True, which='major')
+minorticks_on()
+grid(b=True, which='minor')
+legend(loc="lower left")
+subplot(133, title="Density")
+plot(profile_1[2], depths, label="x=%d, y=%d" % (x_coord_1, y_coord_1))
+plot(profile_2[2], depths, label="x=%d, y=%d" % (x_coord_2, y_coord_2))
+xlabel("Density")
+ylabel("Depth (km)")
+xlim(0, 5000)
+legend(loc='lower left')
+gcf().set_size_inches(13, 6)
+savefig(output_filename, type="png")
---
+++ UCVM/smoothing/smooth.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "string.h"
+
+inline int min(int a, int b) {
+	if (a<b) 
+		return a;
+	return b;
+}
+
+inline int max(int a, int b) {
+	if (a>b) 
+		return a;
+	return b;
+}
+
+void average_point(float* slice, int x, int y, int nx, int ny, int smooth_dist, float (*vals)[3]) {
+	int i, j, k;
+	int x_start, x_stop, y_start, y_stop;
+	//X-dir
+	x_start = max(x-smooth_dist, 0);
+	x_stop = min(x+smooth_dist, nx-1);
+        y_start = max(y-smooth_dist, 0);
+        y_stop = min(y+smooth_dist, ny-1);
+	float tot[3] = {0, 0, 0};
+	int num_vals = 0;
+	//Calclate average using diamond
+	for (i=x_start; i<=x_stop; i++) {
+		for (j=max(y_start+abs(i-x), 0); j<=min(y_stop-abs(i-x), ny-1); j++) {
+			int index = 3*(i*ny + j);
+			for (k=0; k<3; k++) {
+				tot[k] += slice[index+k];
+			}
+			num_vals++;	
+		}
+	}
+	for (i=0; i<3; i++) {
+		(*vals)[i] = tot[i]/((float)num_vals);
+	}
+}
+
+void process_mesh(char* mesh_in, char* pts_file, int nx, int ny, int nz, int smoothing_dist, char* mesh_out) {
+	FILE* fp_in = fopen(mesh_in, "rb");
+	FILE* fp_out = fopen(mesh_out, "wb");
+	FILE* pts_in = fopen(pts_file, "r");
+
+	float* horizontal_slice_in = malloc(sizeof(float)*3*nx*ny);
+	float* horizontal_slice_out = malloc(sizeof(float)*3*nx*ny);
+
+	//Get points
+	int pt_x, pt_y;
+	float lon, lat;
+	float vals[3];
+	int num_pts = 0;
+	int points[1000000][2];
+	while (fscanf(pts_in, "%d %d %f %f\n", &pt_x, &pt_y, &lon, &lat)!=EOF) {
+		points[num_pts][0] = pt_x;
+		points[num_pts][1] = pt_y;
+		num_pts++;
+	}
+	fclose(pts_in);
+
+	int point_index = 0;
+	int i, j, k;
+	//Iterate over horizontal slices
+	//Fast y, x
+	for (i=0; i<nz; i++) {
+		printf("Horizontal slice %d of %d.\n", i, nz);
+		point_index = 0;
+		fread(horizontal_slice_in, sizeof(float), 3*nx*ny, fp_in);
+		for (j=0; j<nx; j++) {
+			//Copy X-strip over, will overwrite with new values
+			memcpy(horizontal_slice_out+3*j*ny, horizontal_slice_in+3*j*ny, 3*sizeof(float)*ny);
+			//any points with this X-value?
+			while (point_index<num_pts && points[point_index][0]==j) {
+                                //Do averaging for point
+                                average_point(horizontal_slice_in, points[point_index][0], points[point_index][1], nx, ny, smoothing_dist, &vals);
+                                //Update point
+                                for (k=0; k<3; k++) {
+                                        horizontal_slice_out[3*(j*ny + points[point_index][1]) + k] = vals[k];
+                                }
+                                //Get next point
+				point_index++;
+			}
+		}
+		//Write slice to file
+		fwrite(horizontal_slice_out, sizeof(float), 3*nx*ny, fp_out);
+	}
+	fflush(fp_out);
+	fclose(fp_out);
+	fclose(fp_in);
+}
+
+
+int main(int argc, char** argv) {
+	if (argc<8) {
+		printf("Usage: %s <AWP-format mesh in> <list of smoothing pts> <nx> <ny> <nz> <smoothing range in pts> <velocity mesh out>", argv[0]);
+		exit(1);
+	}
+
+	char* mesh_in = argv[1];
+	char* pts_file = argv[2];
+	int nx = atoi(argv[3]);
+	int ny = atoi(argv[4]);
+	int nz = atoi(argv[5]);
+	int smoothing_dist = atoi(argv[6]);
+	char* mesh_out = argv[7];
+
+	process_mesh(mesh_in, pts_file, nx, ny, nz, smoothing_dist, mesh_out);
+
+	return 0;
+}
