b04afa9e42d59d872d30de06111fa71e51982363
Scott Callaghan <scottcal@usc.edu>
2016-08-29 15:18:26 +0000

    Changed BLOCK_SIZE_Z to 288

--- AWP-GPU-SGT/src/pmcl3d_cons.h
+++ AWP-GPU-SGT/src/pmcl3d_cons.h
@@ -1,7 +1,8 @@
 #define BLOCK_SIZE_X 2
 #define BLOCK_SIZE_Y 2
 //#define BLOCK_SIZE_Z 256
-#define BLOCK_SIZE_Z 200
+//#define BLOCK_SIZE_Z 200
+#define BLOCK_SIZE_Z 288
 #define align 32
 #define loop  1 
 
