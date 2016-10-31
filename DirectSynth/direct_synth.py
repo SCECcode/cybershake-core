a2ae4de0a34f02fc8c217df099d2b684e2e0f769
Scott Callaghan <scottcal@usc.edu>
2016-10-31 21:51:25 +0000

    explicitly launch memcached, added hints for Blue Waters to reduce congestion

--- DirectSynth/direct_synth.py
+++ DirectSynth/direct_synth.py
@@ -9,18 +9,19 @@ sys.path.append(path_add)
 
 import config
 
+num_nodes = int(os.environ['PBS_NUM_NODES'])
+ppn = int(os.environ['PBS_NUM_PPN'])
 #Start memcached on each node
 memcached_path = "/projects/sciteam/jmz/CyberShake/utils/pegasus_wrappers/invoke_memcached.sh"
-cmd = "aprun -N 1 %s" % memcached_path
+cmd = "aprun -N 1 -n %d %s" % (num_nodes, memcached_path)
+print cmd
 rc = os.system(cmd)
 if rc!=0:
 	print "Error launching memcached.  Will continue with DirectSynth anyway."
 
 #Launch direct_synth
 ds_path = "%s/bin/direct_synth %s" % (sys.path[0], " ".join(sys.argv[1:]))
-num_nodes = int(os.environ['PBS_NUM_NODES'])
-ppn = int(os.environ['PBS_NUM_PPN'])
-cmd = "aprun -n %d -N %d %s" % (num_nodes*ppn, ppn, ds_path)
+cmd = "export MPICH_MPIIO_HINTS=*:romio_cb_read=disable; export APRUN_BALANCED_INJECTION=64; ulimit -c unlimited; export ATP_ENABLED=1; aprun -n %d -N %d %s" % (num_nodes*ppn, ppn, ds_path)
 print cmd
 rc = os.system(cmd)
 sys.exit((rc >> 8) & 0xFF)
