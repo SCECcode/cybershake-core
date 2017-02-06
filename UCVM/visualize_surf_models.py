b67754e6c0a48d9d882671874c7c5ed792d51764
Scott Callaghan <scottcal@usc.edu>
2017-02-06 19:04:32 +0000

    Added additional models.

--- UCVM/visualize_surf_models.py
+++ UCVM/visualize_surf_models.py
@@ -26,6 +26,9 @@ with open(model_coords_file, "r") as fp_in:
 cvms_pts = []
 usgs_pts = []
 oned_pts = []
+cca_pts = []
+cvmsi_pts = []
+other_pts = []
 
 print "Reading surface file."
 with open(surf_file, "r") as fp_in:
@@ -41,6 +44,12 @@ with open(surf_file, "r") as fp_in:
 				usgs_pts.append(coords[i*nx+j])
 			elif pieces[j]=="1d":
 				oned_pts.append(coords[i*nx+j])
+			elif pieces[j]=="cca":
+				cca_pts.append(coords[i*nx+j])
+			elif pieces[j]=="cvmsi":
+				cvmsi_pts.append(coords[i*nx+j])
+			elif pieces[j]=="-1":
+				other_pts.append(coords[i*nx+j])
 			else:
 				print "Label %s not recognized, aborting." % pieces[j]
 				sys.exit(1)
@@ -50,9 +59,14 @@ clf()
 plot([a[0] for a in oned_pts], [a[1] for a in oned_pts], '.', color="black", label="1d")
 plot([a[0] for a in usgs_pts], [a[1] for a in usgs_pts], '.', color="green", label="usgs")
 plot([a[0] for a in cvms_pts], [a[1] for a in cvms_pts], '.', color="red", label="cvms5")
+plot([a[0] for a in cca_pts], [a[1] for a in cca_pts], '.', color="orange", label="cca")
+plot([a[0] for a in cvmsi_pts], [a[1] for a in cvmsi_pts], '.', color="blue", label="cvmsi")
+plot([a[0] for a in other_pts], [a[1] for a in other_pts], '.', color="gray", label="other")
+
+
 legend()
-xlim(-130, -110)
-ylim(30, 42)
+xlim(-125, -113)
+ylim(30, 40)
 xlabel("Latitude")
 ylabel("Longitude")
 gcf().set_size_inches(8,8)
