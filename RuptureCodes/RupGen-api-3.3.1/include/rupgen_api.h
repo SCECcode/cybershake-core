bf767fe4fbc38d21e84c40d6e97e7eb33b9f2865
Scott Callaghan <scottcal@usc.edu>
2017-02-07 03:10:01 +0000

    Updated API with random/uniform hypo, hypo distribution

--- RuptureCodes/RupGen-api-3.3.1/include/rupgen_api.h
+++ RuptureCodes/RupGen-api-3.3.1/include/rupgen_api.h
@@ -7,7 +7,12 @@
 int set_memcached_server(char* memcached_server);
 #endif
 
-int rupgen_get_num_rv(char* rup_geom_file, rg_stats_t *stats);
-int rupgen_genslip(char* rup_geom_file, int slip, int hypo, rg_stats_t *stats, struct standrupformat* srf);
+#define RUPGEN_RANDOM_HYPO 0
+#define RUPGEN_UNIFORM_HYPO 1
+
+int rupgen_get_num_rv(char* rup_geom_file, rg_stats_t *stats, int hypo_dist);
+int rupgen_genslip_with_params(char* rup_geom_file, int slip, int hypo, rg_stats_t *stats, struct standrupformat* srf, int hypo_dist, float dt, char* params);
+int rupgen_genslip(char* rup_geom_file, int slip, int hypo, rg_stats_t *stats, struct standrupformat* srf, int hypo_dist, float dt);
+void free_srf_ptrs(struct standrupformat* srf);
 
 #endif
