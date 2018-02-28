aaac384385a2ec2c6bf31d2c4338f94d49ffd10f
Scott Callaghan <scottcal@usc.edu>
2018-02-28 20:51:25 +0000

    Simple wrapper to create directories

---
+++ HFSim_mem/create_dirs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+if len(sys.argv)<2:
+	print "Usage: %s <file with list of dirs>" % (sys.argv[0])
+	sys.exit(1)
+
+for line in open(sys.argv[1], "r"):
+	os.mkdir(line.strip())
+
+
