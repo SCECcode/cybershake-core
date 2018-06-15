0bcb49888a6b590fb232ec428e414e1245bc145e
Scott Callaghan <scottcal@usc.edu>
2018-06-15 15:48:05 +0000

    Added additional models

--- UCVM/visualize_surf_models.py
+++ UCVM/visualize_surf_models.py
@@ -6,29 +6,31 @@ from pylab import *
 import sys
 import os
 
-if len(sys.argv)<5:
-	print "Usage: %s <surface file> <model coords file> <nx> <ny>" % (sys.argv[0])
+if len(sys.argv)<6:
+	print "Usage: %s <surface file> <model coords file> <nx> <ny> <output file>" % (sys.argv[0])
 	sys.exit(1)
 
 surf_file = sys.argv[1]
 model_coords_file = sys.argv[2]
 nx = int(sys.argv[3])
 ny = int(sys.argv[4])
+output_file = sys.argv[5]
 
 #get list of points
 coords = []
 print "Reading model coods file."
 with open(model_coords_file, "r") as fp_in:
-	for line in fp_in:
+	data = fp_in.readlines()
+	for line in data:
 		pieces = line.split()
 		coords.append([float(pieces[0]), float(pieces[1])])
+	fp_in.close()
 
 cvms_pts = []
 usgs_pts = []
 oned_pts = []
-cca_pts = []
 cvmsi_pts = []
-other_pts = []
+cca_pts = []
 
 print "Reading surface file."
 with open(surf_file, "r") as fp_in:
@@ -44,12 +46,10 @@ with open(surf_file, "r") as fp_in:
 				usgs_pts.append(coords[i*nx+j])
 			elif pieces[j]=="1d":
 				oned_pts.append(coords[i*nx+j])
-			elif pieces[j]=="cca":
-				cca_pts.append(coords[i*nx+j])
 			elif pieces[j]=="cvmsi":
 				cvmsi_pts.append(coords[i*nx+j])
-			elif pieces[j]=="-1":
-				other_pts.append(coords[i*nx+j])
+			elif pieces[j]=="cca":
+				cca_pts.append(coords[i*nx+j])
 			else:
 				print "Label %s not recognized, aborting." % pieces[j]
 				sys.exit(1)
@@ -59,16 +59,13 @@ clf()
 plot([a[0] for a in oned_pts], [a[1] for a in oned_pts], '.', color="black", label="1d")
 plot([a[0] for a in usgs_pts], [a[1] for a in usgs_pts], '.', color="green", label="usgs")
 plot([a[0] for a in cvms_pts], [a[1] for a in cvms_pts], '.', color="red", label="cvms5")
-plot([a[0] for a in cca_pts], [a[1] for a in cca_pts], '.', color="orange", label="cca")
 plot([a[0] for a in cvmsi_pts], [a[1] for a in cvmsi_pts], '.', color="blue", label="cvmsi")
-plot([a[0] for a in other_pts], [a[1] for a in other_pts], '.', color="gray", label="other")
-
-
+plot([a[0] for a in cca_pts], [a[1] for a in cca_pts], '.', color="orange", label="cca")
 legend()
-xlim(-125, -113)
-ylim(30, 40)
+xlim(-127, -112)
+ylim(30, 43)
 xlabel("Latitude")
 ylabel("Longitude")
 gcf().set_size_inches(8,8)
-savefig("surface_plot.png", format="png")
+savefig(output_file, format="png")
 
