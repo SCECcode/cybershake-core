79d2f76d5c98bc950a14c5bd6da7fe971eb58256
Scott Callaghan <scottcal@usc.edu>
2018-07-27 21:32:16 +0000

    Added striping to output file if reformating

--- AWP-GPU-SGT/utils/prepare_for_pp.py
+++ AWP-GPU-SGT/utils/prepare_for_pp.py
@@ -16,7 +16,12 @@ import config
 
 CS_PATH = config.getProperty("CS_PATH")
 
+LFS_PATH = "/opt/cray/lustre-cray_gem_s/2.8.0_3.0.101_0.46.1_1.0502.8871.21.1-1.0502.0.6.1/bin/lfs"
+
 def reformat(input_filename, timesteps, num_pts, output_filename, comp):
+	#First, set up striping on the output file
+	command = "%s setstripe -c 160 -S 5m %s" % (LFS_PATH, output_filename)
+	exitcode = os.system(command)
 	#On Titan, need aprun to execute this on a compute node, not the aprun node
 	command = "aprun -n 4 -N 2 -S 1 %s/SgtHead/bin/reformat_awp_mpi %s %d %d %s" % (CS_PATH, input_filename, timesteps, num_pts, output_filename)
 	if comp=="z":
