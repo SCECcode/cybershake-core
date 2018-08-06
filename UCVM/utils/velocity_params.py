d042ec7aa20a73ceb7d244e2be6b51aa97f6747c
Scott Callaghan <scottcal@usc.edu>
2018-08-06 16:55:32 +0000

    Switched over to using gridout file for grid size

--- UCVM/utils/velocity_params.py
+++ UCVM/utils/velocity_params.py
@@ -8,7 +8,7 @@ import MySQLdb
 import argparse
 
 full_path = os.path.abspath(sys.argv[0])
-path_add = os.path.dirname(os.path.dirname(full_path))
+path_add = os.path.dirname(os.path.dirname(os.path.dirname(full_path)))
 
 sys.path.append(path_add)
 
@@ -20,14 +20,12 @@ parser.add_argument("-lon", "--longitude", help="Longitude of site", type=float)
 parser.add_argument("-m", "--models", help="Comma-separated string of UCVM velocity models",)
 parser.add_argument("-vmesh", "--velocity_mesh", help="Path to velocity mesh file")
 parser.add_argument("-fd", "--fdloc", help="Path to fdloc file with site coordinates")
-parser.add_argument("-nx", help="Number of grid points in the X direction", type=int)
-parser.add_argument("-ny", help="Number of grid points in the Y direction", type=int)
-parser.add_argument("-nz", help="Number of grid points in the Z direction", type=int)
+parser.add_argument("-go", "--gridout", help="Path to gridout file")
 parser.add_argument("-s", "--server", help="Database server to use")
 parser.add_argument("-r", "--run_id", help="Run ID")
 
 args = parser.parse_args()
-if args.latitude==None or args.longitude==None or args.models==None or args.velocity_mesh==None or args.nx==None or args.ny==None or args.nz==None or args.fdloc==None or args.run_id==None:
+if args.latitude==None or args.longitude==None or args.models==None or args.velocity_mesh==None or args.gridout==None or args.fdloc==None or args.run_id==None:
 	print "Missing required arguments."
 	parser.print_help()
 	sys.exit(1)
@@ -38,16 +36,29 @@ if args.server is not None:
 
 #Call get_model_info_for_db to get model Vs30, Z1.0, Z2.5
 model_output_file = "ucvm_data.txt"
-cmd = "%s/UCVM/bin/get_model_info_for_db %f %f %s %s" % (config.getProperty("CS_PATH")), args.latitude, args.longitude, args.models, model_output_file)
+UCVM_HOME = "/projects/sciteam/baln/CyberShake/software/UCVM/ucvm-18.5.0_07282018"
+ld_lib_path="%s/lib/euclid3/lib:%s/lib/proj-4/lib:%s/model/cvms426/lib:%s/model/cencal/lib:%s/model/cvms5/lib:%s/model/cca/lib" % (UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME)
+cmd = "export LD_LIBRARY_PATH=%s:LD_LIBRARY_PATH; %s/UCVM/bin/get_model_info_for_db %f %f %s %s" % (ld_lib_path, config.getProperty("CS_PATH"), args.latitude, args.longitude, args.models, model_output_file)
+print cmd
 os.system(cmd)
+
+#Determine mesh size
+with open(args.gridout, "r") as fp_in:
+	data = fp_in.readlines()
+	nx = int(data[1].split("=")[1].strip())
+	ny = int(data[1+nx+2].split("=")[1].strip())
+	nz = int(data[1+nx+2+ny+2].split("=")[1].strip())
+	print nx, ny, nz
+	fp_in.close()
 
 #Run value_from_mesh
 with open(args.fdloc, "r") as fp_in:
-	[x_index, y_index] = fp_in.readline().split()
+	[x_index, y_index] = [int(x) for x in fp_in.readline().split()]
 	fp_in.close()
 z_index = 0
 mesh_output_file = "mesh_data.txt"
-cmd = "%s/UCVM/utils/value_from_mesh %s %d %d %d %d %d %d > %s" % (args.velocity_mesh, args.nx, args.ny, args.nz, x_index, y_index, z_index, mesh_output_file)
+cmd = "%s/UCVM/utils/value_from_mesh %s %d %d %d %d %d %d > %s" % (config.getProperty("CS_PATH"), args.velocity_mesh, nx, ny, nz, x_index, y_index, z_index, mesh_output_file)
+print cmd
 os.system(cmd)
 
 with open(model_output_file, "r") as fp_in:
@@ -62,6 +73,8 @@ with open(mesh_output_file, "r") as fp_in:
 
 conn = MySQLdb.connect(host=server, db="CyberShake", user="cybershk", passwd='re@lStil1')
 cur = conn.cursor()
-update = "update CyberShake_Runs set Model_Vs30=%f, Mesh_Vs30=%f, Z1_0=%f, Z2_5=%f where Run_ID=%d" % (model_vs30, mesh_vs30, model_z10, model_z25, run_id)
-cur.query(update)
-
+update = "update CyberShake_Runs set Model_Vs30=%f, Mesh_Vs30=%f, Z1_0=%f, Z2_5=%f where Run_ID=%d" % (model_vs30, mesh_vs, model_z10, model_z25, int(args.run_id))
+print update
+cur.execute(update)
+conn.commit()
+cur.close()
