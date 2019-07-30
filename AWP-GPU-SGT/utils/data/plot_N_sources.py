d68ec9792d974d67fea8d239f3da7e65b4adada1
Scott Callaghan <scottcal@usc.edu>
2019-07-30 18:39:12 +0000

    Added max Ts as arguments

--- AWP-GPU-SGT/utils/data/plot_N_sources.py
+++ AWP-GPU-SGT/utils/data/plot_N_sources.py
@@ -4,22 +4,23 @@ import sys
 import os
 import numpy
 import matplotlib
+matplotlib.use("AGG", warn=False)
 from pylab import *
 
 if len(sys.argv)<4 or len(sys.argv)%2 != 0:
-	print "Usage: %s <source file 1> <label 1> <source file 2> <label 2> ... <source file N> <label N> <output file>" % sys.argv[0]
+	print "Usage: %s <source file 1> <label 1> <maxt 1> <source file 2> <label 2> <maxt 2> ... <source file N> <label N> <output file>" % sys.argv[0]
 	sys.exit(1)
 
 filenames = []
 labels = []
-for f in range(1, len(sys.argv)-1, 2):
+maxts = []
+for f in range(1, len(sys.argv)-1, 3):
 	filenames.append(sys.argv[f])
 	labels.append(sys.argv[f+1])
+	maxts.append(float(sys.argv[f+2]))
 
 outfile = sys.argv[-1]
 
-#Assume all sources have the same length
-MAXT = 200.0
 
 for j in range(0, len(filenames)):
 	f = filenames[j]
@@ -31,13 +32,13 @@ for j in range(0, len(filenames)):
 		pieces = line.split(",")
 	        data_vals.append(float(pieces[0].strip()))
 	nt = len(data_vals)
-	dt = MAXT/nt
+	dt = maxts[j]/nt
 	print "Using dt=%f, nt=%d" % (dt, nt)
 	timesteps = [dt*i for i in range(0, nt)]
 	plot(timesteps, data_vals, label=labels[j])
 
 legend(loc="upper right")
-xlim(0, MAXT/20.0)
+xlim(0, 3.0)
 gcf().set_size_inches(8,6)
 savefig(outfile, format="png")
 
