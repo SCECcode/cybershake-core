cbb6edd44c3b6f108d92023e61151edb311ea752
Scott Callaghan <scottcal@usc.edu>
2020-02-12 20:35:41 +0000

    Utilities to get the number of rupture variations for an erf/src/rup combo, and to generate hypocenters for all the rupture variations for an erf/src/rup combo

---
+++ RuptureCodes/RupGen-api-5.4.2/utils/get_num_rvs.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "rupgen_api.h"
+
+const char* RUPTURE_ROOT = "/gpfs/alpine/proj-shared/geo112/CyberShake/ruptures";
+
+int main(int argc, char** argv) {
+	set_memcached_server("localhost");
+	if (argc<4) {
+		printf("Usage: %s <erf id> <src id> <rup id>\n", argv[0]);
+		exit(1);
+	}
+	int erf = atoi(argv[1]);
+	int src = atoi(argv[2]);
+	int rup = atoi(argv[3]);
+	char rupture_file[512];
+	sprintf(rupture_file, "%s/Ruptures_erf%d/%d/%d/%d_%d.txt", RUPTURE_ROOT, erf, src, rup, src, rup);
+	rg_stats_t stats;
+	int slip, hypo;
+	rupgen_get_num_rv(rupture_file, &stats, RUPGEN_UNIFORM_HYPO);
+	printf("%d\n", stats.numslip*stats.numhypo);
+	return 0;
+}
