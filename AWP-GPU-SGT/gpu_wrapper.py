45240d8b28d357e7376534ca188f2f7a5112e951
Scott Callaghan <scottcal@usc.edu>
2020-03-20 20:18:44 +0000

    Added jsrun logic

--- AWP-GPU-SGT/gpu_wrapper.py
+++ AWP-GPU-SGT/gpu_wrapper.py
@@ -30,13 +30,19 @@ for line in in3d_data:
 			value = value.split("'")[1]
 		params[key] = value
 		
-module_cmd = "/opt/modules/3.2.6.7/bin/modulecmd bash swap PrgEnv-cray PrgEnv-gnu; /opt/modules/3.2.6.7/bin/modulecmd bash load cudatoolkit"
+#module_cmd = "/opt/modules/3.2.6.7/bin/modulecmd bash swap PrgEnv-cray PrgEnv-gnu; /opt/modules/3.2.6.7/bin/modulecmd bash load cudatoolkit"
 mpi_cmd = config.getProperty("MPI_CMD")
 nproc = int(params["NPX"])*int(params["NPY"])*int(params["NPZ"])
 path = "%s/AWP-GPU-SGT/bin/pmcl3d" % config.getProperty("CS_PATH")
 
+suffix = "%s -T %s --IGREEN %s --DH %s --DT %s --NSRC %s --NST %s -X %s -Y %s -Z %s -x %s -y %s --NPC %s --ARBC %s --ND %s --IFAULT %s --MEDIASTART %s --FL %s --FH %s --FP %s --READ_STEP %s --WRITE_STEP %s -o %s --INSGT %s -c %s --INSRC %s --INVEL %s --NTISKP_SGT %s --NTISKP %s" % (path, params["TMAX"], params["igreen"], params["DH"], params["DT"], params["NSRC"], params["NST"], params["NX"], params["NY"], params["NZ"], params["NPX"], params["NPY"], params["NPC"], params["ARBC"], params["ND"], params["IFAULT"], params["MEDIARESTART"], params["FL"], params["FH"], params["FP"], params["READ_STEP"], params["WRITE_STEP"], params["SGTGRO"].rsplit("/", 1)[0], params["INSGT"], params["CHKP"], params["INSRC"], params["INVEL"], params["NTISKP_SGT"], params["NTISKP"])
 
-exec_string = "export APRUN_BALANCED_INJECTION=64; %s -n %d -N 1 %s -T %s --IGREEN %s --DH %s --DT %s --NSRC %s --NST %s -X %s -Y %s -Z %s -x %s -y %s --IFAULT %s --MEDIASTART %s --FL %s --FH %s --FP %s --READ_STEP %s --WRITE_STEP %s -o %s --INSGT %s -c %s --INSRC %s --INVEL %s --NTISKP_SGT %s --NTISKP %s" % (mpi_cmd, nproc, path, params["TMAX"], params["igreen"], params["DH"], params["DT"], params["NSRC"], params["NST"], params["NX"], params["NY"], params["NZ"], params["NPX"], params["NPY"], params["IFAULT"], params["MEDIARESTART"], params["FL"], params["FH"], params["FP"], params["READ_STEP"], params["WRITE_STEP"], params["SGTGRO"].rsplit("/", 1)[0], params["INSGT"], params["CHKP"], params["INSRC"], params["INVEL"], params["NTISKP_SGT"], params["NTISKP"])
+if mpi_cmd=='aprun':
+	exec_string = "export APRUN_BALANCED_INJECTION=64; aprun -n %d -N 1 %s" % (nproc, suffix)
+elif mpi_cmd=='jsrun':
+	#1 GPU, 1 CPU, 1 MPI rank per resource set; 6 RSs per node, but we don't specify -r because
+	#we are OK with different numbers of RSs per node
+	exec_string = "jsrun -n %d -g 1 -a 1 -c 1 %s" % (nproc, suffix)
 
 #run exec_string
 print exec_string
