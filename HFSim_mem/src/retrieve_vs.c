f1aa8013ac2bc12e66ee6e42879b774dc7c2bedc
Scott Callaghan <scottcal@usc.edu>
2020-03-31 20:56:30 +0000

    Added support for additional models and multiple models

--- HFSim_mem/src/retrieve_vs.c
+++ HFSim_mem/src/retrieve_vs.c
@@ -9,32 +9,45 @@ const float MIN_VS = 500.0;
 void initialize_ucvm(char* model) {
 	printf("Initializing UCVM.\n");
 	if (ucvm_init("/gpfs/alpine/proj-shared/geo112/CyberShake/software/UCVM/ucvm-18.5.0_01302019/conf/ucvm.conf")!=UCVM_CODE_SUCCESS) {
-                        fprintf(stderr, "Failed to init UCVM, aborting.");
-                        exit(3);
-                }
+		fprintf(stderr, "Failed to init UCVM, aborting.");
+        exit(3);
+    }
 
-                char* model_string;
-                if (strcmp(model, "cvmh")==0) { 
-                        model_string = UCVM_MODEL_CVMH;
-                } else if (strcmp(model, "cvms")==0) {
-                        model_string = UCVM_MODEL_CVMS;
-                } else if (strcmp(model, "1d")==0 || strcmp(model, "scec1d")==0) {
-                        model_string = UCVM_MODEL_1D;
-                } else if (strcmp(model, "cvmsi")==0) {
-                        model_string = UCVM_MODEL_CVMSI;
-                } else if (strcmp(model, "bbp1d")==0) {
-                        model_string = UCVM_MODEL_BBP1D;
-                }
-                printf("Adding model %s.\n", model_string);
-                if (ucvm_add_model(model_string)!=UCVM_CODE_SUCCESS) {
-                        fprintf(stderr, "Error retrieving UCVM model %s.", model_string);
-                        exit(3);
-                }
+	//Model might be a comma-separated list of models; load them all
+	char* tok;
+	tok = strtok(model, ",");
+	while (tok!=NULL) {
+	    char* model_string;
+        if (strcmp(tok, "cvmh")==0) { 
+    	    model_string = UCVM_MODEL_CVMH;
+        } else if (strcmp(tok, "cvms")==0) {
+            model_string = UCVM_MODEL_CVMS;
+        } else if (strcmp(tok, "1d")==0 || strcmp(model, "scec1d")==0) {
+            model_string = UCVM_MODEL_1D;
+        } else if (strcmp(tok, "cvmsi")==0) {
+            model_string = UCVM_MODEL_CVMSI;
+        } else if (strcmp(tok, "bbp1d")==0) {
+            model_string = UCVM_MODEL_BBP1D;
+        } else if (strcmp(tok, "usgs")==0) {
+			model_string = UCVM_MODEL_CENCAL;
+		} else if (strcmp(tok, "cca")==0) {
+			model_string = "cca";
+		} else {
+			fprintf(stderr, "Don't recognize model %s.  Aborting.\n", tok);
+			exit(1);
+		}
+        printf("Adding model %s.\n", model_string);
+        if (ucvm_add_model(model_string)!=UCVM_CODE_SUCCESS) {
+        	fprintf(stderr, "Error retrieving UCVM model %s.", model_string);
+            exit(3);
+        }
 
-                if (ucvm_setparam(UCVM_PARAM_QUERY_MODE, UCVM_COORD_GEO_DEPTH)!=UCVM_CODE_SUCCESS) {
-                        fprintf(stderr, "Set query mode by depth failed.\n");
-                        exit(-2);
-                }
+        if (ucvm_setparam(UCVM_PARAM_QUERY_MODE, UCVM_COORD_GEO_DEPTH)!=UCVM_CODE_SUCCESS) {
+        	fprintf(stderr, "Set query mode by depth failed.\n");
+            exit(-2);
+        }
+		tok = strtok(NULL, ",");
+	}
 	printf("Initialization complete.\n");
 }
 
