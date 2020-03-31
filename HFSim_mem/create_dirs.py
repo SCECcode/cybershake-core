145cf2308a14caf7fa5acaafd9e357b4520944a5
Scott Callaghan <scottcal@usc.edu>
2020-03-31 20:59:01 +0000

    Updated to python 3

--- HFSim_mem/create_dirs.py
+++ HFSim_mem/create_dirs.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import os
 
 if len(sys.argv)<2:
-	print "Usage: %s <file with list of dirs>" % (sys.argv[0])
+	print("Usage: %s <file with list of dirs>" % (sys.argv[0]))
 	sys.exit(1)
 
 for line in open(sys.argv[1], "r"):
