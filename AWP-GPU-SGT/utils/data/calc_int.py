42940dad3797e90f9331fb018daa2fa851fa8183
Scott Callaghan <scottcal@usc.edu>
2021-12-10 23:29:37 +0000

    Converted to Python3

--- AWP-GPU-SGT/utils/data/calc_int.py
+++ AWP-GPU-SGT/utils/data/calc_int.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 if len(sys.argv)<3:
-	print "Usage: %s <source file> <dt>" % sys.argv[0]
+	print("Usage: %s <source file> <dt>" % sys.argv[0])
 	sys.exit(1)
 
 dt = float(sys.argv[2])
@@ -16,4 +16,4 @@ with open(sys.argv[1], "r") as fp_in:
 		pieces = line.split(",")
 		sum += float(pieces[0])/1.0e15*dt
 
-print sum
+print(sum)
