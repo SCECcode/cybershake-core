a06931ed2f331b34a4b673a9e9346fbdc6702f1e
Scott Callaghan <scottcal@usc.edu>
2021-12-10 18:00:13 +0000

    Python3 changes; changed default PX=PY=20 for Study 21.12; default padding to 50 km.

--- PreCVM/Modelbox/get_modelbox.py
+++ PreCVM/Modelbox/get_modelbox.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python3
 
 """
 This program inputs the ruptures that are relevant to a particular 
@@ -47,9 +47,9 @@ def opendb(h,pt,us,pwd,d,sqlite=False):
     try:
       connection = pymysql.connect(host=h,port=pt,user=us,passwd=pwd,db=d)
       return connection
-    except pymysql.Error,message:
+    except pymysql.Error as message:
       errorMessage = "Error %d:\n%s" % (message [ 0],message[1])
-      print errorMessage
+      print(errorMessage)
       sys.exit(-1)
   else:
     print(h)
@@ -60,9 +60,9 @@ def closedb(conn):
   try:
     conn.close()
     return	
-  except pymysql.Error,message:
+  except pymysql.Error as message:
     errorMessage = "Error %d:\n%s" % (message [ 0],message[1])
-    print errorMessage
+    print(errorMessage)
     sys.exit(-1)
 
 #
@@ -73,7 +73,7 @@ pi= 3.14159
 kplat = 111.19
 model_rot = -55.0
 #model_rot = -30.0
-bound_pad = 30.0
+bound_pad = 50.0
 xmin = 1.0e15
 xmax = -1.0e15
 ymin = 1.0e15
@@ -86,9 +86,9 @@ pwd = "CyberShake2007"
 port = 3306
 db="CyberShake"
 
-if len(sys.argv) < 6:
-  print "Syntax: get_modelbox.py SITE_Name ERF_ID Outfile_name spacing(in km) Server rotation [gpu] [bbox] [tight]"
-  print "Example: get_modelbox.py USC 34 ./usc_modelbox.txt 0.2 focal.usc.edu -55"
+if len(sys.argv) < 7:
+  print("Syntax: get_modelbox.py SITE_Name ERF_ID Outfile_name spacing(in km) Server rotation [padding] ['gpu'] ['bbox'] ['tight']")
+  print("Example: get_modelbox.py USC 34 ./usc_modelbox.txt 0.2 focal.usc.edu -55")
   sys.exit()
 
 site = sys.argv[1]
@@ -103,20 +103,24 @@ if host[0:9]=="sqlite://":
     host = host[9:]
 
 model_rot = float(sys.argv[6])
+if len(sys.argv) >= 8:
+    padding = float(sys.argv[7])
+    if padding>0.0:
+        bound_pad = padding
 
 gpu = False
-if len(sys.argv) >= 7 and "gpu" in sys.argv[7:]:
-	print "GPU mode."
+if len(sys.argv) >= 9 and "gpu" in sys.argv[8:]:
+	print("GPU mode.")
 	gpu = True
 
 bbox = False
-if len(sys.argv) >= 7 and "bbox" in sys.argv[7:]:
-	print "Bounding box mode."
+if len(sys.argv) >= 9 and "bbox" in sys.argv[8:]:
+	print("Bounding box mode.")
 	bbox = True
 
 tight = False
-if len(sys.argv) >= 7 and "tight" in sys.argv[7:]:
-	print "Tight (20 km spacing) mode."
+if len(sys.argv) >= 9 and "tight" in sys.argv[8:]:
+	print("Tight (20 km spacing) mode.")
 	tight = True
 
 if tight:
@@ -134,13 +138,13 @@ cur = conn.cursor()
 sql_string = "select CS_Site_Lon, CS_Site_Lat, CS_Site_ID from CyberShake_Sites where CS_Short_Name='%s'" % site
 try:
   cur.execute(sql_string)
-except pymysql.DatabaseError,e:
-  print e
+except pymysql.DatabaseError as e:
+  print(e)
   sys.exit(-1)
   
 res = cur.fetchall()
 if len(res)==0:
-   print "Unable to find station %s in DB. Exiting\n" % site
+   print("Unable to find station %s in DB. Exiting\n" % site)
    sys.exit(-1)
 
 site_lon = float(res[0][0])
@@ -150,8 +154,8 @@ site_id = int(res[0][2])
 sql_string = "select distinct Ruptures.Source_ID,Ruptures.Rupture_ID,Source_Name,Mag,Prob,Start_Lat,Start_Lon,End_Lat,End_Lon from Ruptures,CyberShake_Site_Ruptures where Ruptures.ERF_ID=%s and Ruptures.ERF_ID=CyberShake_Site_Ruptures.ERF_ID and CyberShake_Site_Ruptures.CS_Site_ID=%s and CyberShake_Site_Ruptures.Source_ID=Ruptures.Source_ID and CyberShake_Site_Ruptures.Rupture_ID=Ruptures.Rupture_ID order by Mag desc"%(erf,site_id)
 try:
   cur.execute(sql_string)
-except pymysql.DatabaseError,e:
-  print e
+except pymysql.DatabaseError as e:
+  print(e)
   sys.exit(-1)
 
 count = cur.rowcount
@@ -213,14 +217,14 @@ for flts in faults:
   clat = clat+flts.mxlat
   
   if bbox:
-	#Add the other two corners of the box defined by (mnlat, mnlon) and (mxlat, mxlon)
-	np = np+1
-	lon.append(flts.mnlon)
-	lat.append(flts.mxlat)
-	clon = clon+flts.mnlon
-	clat = clat+flts.mxlat
+        #Add the other two corners of the box defined by (mnlat, mnlon) and (mxlat, mxlon)
+        np = np+1
+        lon.append(flts.mnlon)
+        lat.append(flts.mxlat)
+        clon = clon+flts.mnlon
+        clat = clat+flts.mxlat
 
-	np = np+1
+        np = np+1
         lon.append(flts.mxlon)
         lat.append(flts.mnlat)
         clon = clon+flts.mxlon
@@ -251,7 +255,7 @@ for i in range(0,np):
   if ym[i]>ymax:
     ymax=ym[i]
 
-print xmax, xmin, ymax, ymin
+print(xmax, xmin, ymax, ymin)
 xmax = xmax+bpad
 xmin = xmin-bpad
 ymax = ymax+bpad
@@ -269,7 +273,7 @@ e = (y0*cosR) + (x0*sinR)
 mlon=clon + (e/kplon)
 mlat=clat + (n/kplat)
 
-print "First lengths: %f, %f" % (xlen, ylen)
+print("First lengths: %f, %f" % (xlen, ylen))
 
 #Round up to nearest 10km
 xlrnd=10.0*int((xlen/10.0) + 0.5)
@@ -278,7 +282,7 @@ ylrnd=10.0*int((ylen/10.0) + 0.5)
 
 if gpu:
 	#Round up so integral number of grid points in each dimension
-        print "Old lengths: %f, %f" % (xlrnd, ylrnd)
+        print("Old lengths: %f, %f" % (xlrnd, ylrnd))
         if spacing<0:
                 spacing = 0.1
         nx = int(float(xlrnd)/spacing)
@@ -291,19 +295,30 @@ if gpu:
                 ylrnd += spacing - (float(ylrnd)-ny*spacing)
                 ny += 1
 	#Want a rule so we use (in AWP coords) 10x10x1 for 0.5 Hz/200m meshes, 20x10x1 for 1Hz/175m meshes, 40x20x1 for 1 Hz/100m meshes; will use spacing for now
-	print "Spacing = %f" % spacing
-	if spacing==0.2:
-		print "Spacing = 0.2"
-		xlrnd += spacing*((-1*nx) % 20)
-		ylrnd += spacing*((-1*ny) % 20)
-	elif spacing==0.1:
-		#Changed to 40x30x1 for Study 18.8, since some volumes are large and 300 sec
-                xlrnd += spacing*((-1*nx) % 60)
-                ylrnd += spacing*((-1*ny) % 80)
-	else:
+        print("Spacing = %f" % spacing)
+        if spacing==0.2:
+	        print("Spacing = 0.2")
+	        xlrnd += spacing*((-1*nx) % 20)
+	        ylrnd += spacing*((-1*ny) % 20)
+        elif spacing==0.1:
+	        #Changed to 40x30x1 for Study 18.8, since some volumes are large and 300 sec
+                #Try 20x20x1 for Summit RSQSim testing
+                #xlrnd += spacing*((-1*nx) % 60)
+                #ylrnd += spacing*((-1*ny) % 80)
+                xlrnd += spacing*((-1*nx) % 40)
+                ylrnd += spacing*((-1*ny) % 40)
+        elif spacing==0.025:
+	        xlrnd += spacing*((-1*nx) % 160)
+	        ylrnd += spacing*((-1*ny) % 160)
+        elif math.fabs(spacing-0.0333)<0.001:
+	        xlrnd += spacing*((-1*nx) % 80)
+	        ylrnd += spacing*((-1*ny) % 80)
+                #xlrnd = spacing*3840
+                #ylrnd = spacing*3840
+        else:
                 xlrnd += spacing*((-1*nx) % 40)
                 ylrnd += spacing*((-1*ny) % 80)
-        print "New lengths: %f, %f" % (xlrnd, ylrnd)
+        print("New lengths: %f, %f" % (xlrnd, ylrnd))
 else:
 	#Pad so that X and Y dims are divisible by the gridspacing, if specified
 	if spacing>0:
@@ -313,7 +328,7 @@ else:
 		ny = int(float(ylrnd)/spacing)
 		if ny*spacing!=float(ylrnd):
 			ylrnd += spacing - (float(ylrnd)-ny*spacing)
-		print "Padded to %f, %f to be evenly divisible by the requested spacing, %f km" % (xlrnd, ylrnd, spacing)
+		print("Padded to %f, %f to be evenly divisible by the requested spacing, %f km" % (xlrnd, ylrnd, spacing))
 
 
 f.write("APPROXIMATE CENTROID:\n")
@@ -338,7 +353,7 @@ for x,lin in enumerate(gcout):
     vals.append(mb)
   
 if len(vals) != 4:
-  print "gcproj did not return expected values"
+  print("gcproj did not return expected values")
   sys.exit(-1)
 
 for vs in vals: 
