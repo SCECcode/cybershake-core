6f38a880856e99345a5ff71074b176b5272ee6ae
Scott Callaghan <scottcal@usc.edu>
2021-12-10 23:42:10 +0000

    Converted to Python3

--- CheckSgt/CheckSgt.py
+++ CheckSgt/CheckSgt.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import os
-import md5
+import hashlib
 
 
 # Global vars
@@ -19,23 +19,23 @@ def init():
     
     # Parse command line arguments
     if (argc < 3):
-        print "Usage: " + sys.argv[0] + " <sgt file> <md5 file>"
-        print "Example: " + sys.argv[0] + " USC_fx.sgt USC_fx.sgt.md5"
+        print("Usage: " + sys.argv[0] + " <sgt file> <md5 file>")
+        print("Example: " + sys.argv[0] + " USC_fx.sgt USC_fx.sgt.md5")
         return 1
             
     sgtfile = sys.argv[1]
     md5file = sys.argv[2]
 
-    print "Configuration:"
-    print "SGT File:\t" + sgtfile
-    print "MD5 File:\t" + md5file + "\n"
+    print("Configuration:")
+    print("SGT File:\t" + sgtfile)
+    print("MD5 File:\t" + md5file + "\n")
     
     # Check that the files exist
     if (not os.path.isfile(sgtfile)):
-        print "SGT file " + sgtfile + " not found"
+        print("SGT file " + sgtfile + " not found")
         return 1
     if (not os.path.isfile(md5file)):
-        print "MD5 file " + md5file + " not found"
+        print("MD5 file " + md5file + " not found")
         return 1  
     
     return 0
@@ -50,17 +50,17 @@ def main():
         oldmd5val = line.split(" ")[0]
         oldmd5file.close()
         if (len(oldmd5val) != 32):
-            print "Invalid md5sum found: " + oldmd5val
+            print("Invalid md5sum found: " + oldmd5val)
             return 1
     except:
-        print "Unable to read " + md5file
+        print("Unable to read " + md5file)
         return 1
     
-    print "Old md5sum: " + oldmd5val
+    print("Old md5sum: " + oldmd5val)
     
     # Read in the SGT file and compute new md5
-    m = md5.new()
-    sgt = open(sgtfile, 'r') # open in binary mode
+    m = hashlib.md5()
+    sgt = open(sgtfile, 'rb') # open in binary mode
     while True:
         buf = sgt.read(1024)
         if len(buf) == 0:
@@ -68,11 +68,11 @@ def main():
         m.update(buf)
 
     newmd5val = m.hexdigest()
-    print "New md5sum: " +  newmd5val
+    print("New md5sum: " +  newmd5val)
 
     # Compare the old and new md5 values
     if (oldmd5val != newmd5val):
-        print "md5 checksums do not match!"
+        print("md5 checksums do not match!")
         return 1
     
     return 0
