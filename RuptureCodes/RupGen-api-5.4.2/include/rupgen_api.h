fff815da92e3c08beb4b9fcd55d78fb94da3ca2a
Scott Callaghan <scottcal@usc.edu>
2021-12-11 05:22:31 +0000

    Added functions to pass thru seed.

--- RuptureCodes/RupGen-api-5.4.2/include/rupgen_api.h
+++ RuptureCodes/RupGen-api-5.4.2/include/rupgen_api.h
@@ -9,6 +9,7 @@ int set_memcached_server(char* memcached_server);
 
 #define RUPGEN_RANDOM_HYPO 0
 #define RUPGEN_UNIFORM_HYPO 1
+#define RUPGEN_PROVIDE_HYPO 2
 
 /* Rupture generation statistics */
 typedef struct rg_stats_t {
@@ -20,6 +21,9 @@ int rupgen_get_num_rv(char* rup_geom_file, rg_stats_t *stats, int hypo_dist);
 int rupgen_get_num_rv_with_params(char* rup_geom_file, rg_stats_t *stats, int hypo_dist, char* params);
 int rupgen_genslip_with_params(char* rup_geom_file, int slip, int hypo, rg_stats_t *stats, struct standrupformat* srf, int hypo_dist, float dt, char* params);
 int rupgen_genslip(char* rup_geom_file, int slip, int hypo, rg_stats_t *stats, struct standrupformat* srf, int hypo_dist, float dt);
+int rupgen_genslip_seed(char* rup_geom_file, int slip, int hypo, rg_stats_t *stats, struct standrupformat* srf, int hypo_dist, float dt, int seed);
 void free_srf_ptrs(struct standrupformat* srf);
+int rupgen_get_rupture_seed(int src_id, int rup_id);
+int rupgen_get_variation_seed(char* rup_geom_file, rg_stats_t *stats, int hypo_dist, int src_id, int rup_id, int slip, int hypo);
 
 #endif
