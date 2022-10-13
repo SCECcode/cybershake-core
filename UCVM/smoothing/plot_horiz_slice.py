1acfc5697146c0bc16427d0ee5d57291094b9c43
Scott Callaghan <callag@login2.summit.olcf.ornl.gov>
2022-10-13 17:14:49 -0400

    Modified to Python3; changed to log color scale to better show differences in the near-surface.

--- UCVM/smoothing/plot_horiz_slice.py
+++ UCVM/smoothing/plot_horiz_slice.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import matplotlib
 matplotlib.use("AGG", warn=False)
@@ -16,7 +16,7 @@ from mpl_toolkits.basemap import cm
 from operator import itemgetter
 
 if len(sys.argv)<8:
-	print "Usage: %s <velocity file> <model coords file> <nx> <ny> <nz> <z-depth> <decimation> <output file> [vp|vs|rho]" % sys.argv[0]
+	print("Usage: %s <velocity file> <model coords file> <nx> <ny> <nz> <z-depth> <decimation> <output file> [vp|vs|rho]" % sys.argv[0])
 	sys.exit(1)
 
 
@@ -37,11 +37,11 @@ x_dim = int(math.ceil(float(nx)/float(decimation)))
 y_dim = int(math.ceil(float(ny)/float(decimation)))
 
 coords = []
-print "Reading model coods file."
+print("Reading model coods file.")
 with open(model_coords_file, "r") as fp_in:
 	data = fp_in.readlines()
 	for y in range(0, ny, decimation):
-		print "%d of %d y-points" % (y, ny)
+		print("%d of %d y-points" % (y, ny))
 		for x in range(0, nx, decimation):
 			line_index = x + y*nx
 			line = data[line_index]
@@ -51,7 +51,7 @@ with open(model_coords_file, "r") as fp_in:
 slice_floats = []
 vel_data = []
 
-print "Reading velocity file."
+print("Reading velocity file.")
 with open(velocity_file, "rb") as fp_in:
 	#Seek to z-slice
 	offset = zslice*12*nx*ny
@@ -59,7 +59,7 @@ with open(velocity_file, "rb") as fp_in:
 	slice_data = fp_in.read(nx*ny*3*4)
 	#Plot Vs
 	for i in range(0, nx, decimation):
-		print "%d of %d x-slices" % (i, nx)
+		print("%d of %d x-slices" % (i, nx))
 		offset = i*ny*3*4
 		slice_floats = (struct.unpack("%df" % 3*ny, slice_data[offset:offset+ny*3*4]))
 		if comp=="vp":
@@ -72,24 +72,26 @@ with open(velocity_file, "rb") as fp_in:
 ordered_coords = sorted(coords, key=itemgetter(2,3))
 
 #Plotting code taken from UCVM horizontal slice
-BOUNDS = [0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0, 4500.0, 5000.0]
+#BOUNDS = [0.0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0, 4500.0, 5000.0]
+BOUNDS = [500.0, 1000.0, 1500.0, 2000.0, 2500.0]
 #BOUNDS = [0, 3000.0]
-TICKS = [0, 500.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0, 4500.0, 5000.0]
+#TICKS = [500.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0, 4500.0, 5000.0]
+TICKS = [500.0, 1000.0, 1500.0, 2000.0, 2500.0]
 #TICKS = [0, 300.0, 600.0, 900.0, 1200.0, 1500.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0]
 
 #colormap = cm.RdBu
 #Use new color map from SRL paper
 colormap = basemap.cm.GMT_seis
-norm = mcolors.Normalize(vmin=BOUNDS[0],vmax=BOUNDS[len(BOUNDS) - 1])
-
+#norm = mcolors.Normalize(vmin=BOUNDS[0],vmax=BOUNDS[len(BOUNDS) - 1])
+norm = mcolors.LogNorm(vmin=BOUNDS[0],vmax=BOUNDS[len(BOUNDS)-1])
 
 #statewide: 30, 42, -130, -113
-#so cal: 34, 36, -123, -121
+#so cal: 31, 36, -121, -115
 
-min_lat=30
-max_lat=42
-min_lon=-130
-max_lon=-113
+min_lat=31
+max_lat=36
+min_lon=-121
+max_lon=-115
 
 m = basemap.Basemap(projection='cyl', llcrnrlat=min_lat, urcrnrlat=max_lat, llcrnrlon=min_lon, urcrnrlon=max_lon, resolution='f', anchor='C')
 #m = basemap.Basemap(projection='cyl', llcrnrlat=34, urcrnrlat=36, llcrnrlon=-123, urcrnrlon=-121, resolution='f', anchor='C')
@@ -162,6 +164,6 @@ for i in range(0, len(y_vals)):
 m.drawcoastlines()
 
 cax = plt.axes([0.125, 0.05, 0.775, 0.02])
-cbar = plt.colorbar(cax=cax, orientation='horizontal', ticks=TICKS)
-
+cbar = plt.colorbar(cax=cax, orientation='horizontal', ticks=TICKS, format="%d")
+gcf().set_size_inches(10, 8)
 plt.savefig(output_file, type="png")
