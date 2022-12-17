41e41c3787562ce9b0e87c99bba81029629bc139
Scott Callaghan <callag@login5.summit.olcf.ornl.gov>
2022-12-16 22:18:55 -0500

    Added ability to pass in rvfrac.

--- RuptureCodes/RupGen-api-5.4.2/test/test_with_seed.c
+++ RuptureCodes/RupGen-api-5.4.2/test/test_with_seed.c
@@ -5,8 +5,8 @@
 #include "rupgen_api.h"
 
 int main(int argc, char** argv) {
-	if (argc<5) {
-		printf("Usage: %s <erf> <src> <rup> <rv> <seed>\n", argv[0]);
+	if (argc<6) {
+		printf("Usage: %s <erf> <src> <rup> <rv> <seed> <rvfrac>\n", argv[0]);
 		exit(1);
 	}
 	int erf_id = atoi(argv[1]);
@@ -14,6 +14,7 @@ int main(int argc, char** argv) {
 	int rup = atoi(argv[3]);
 	int rv = atoi(argv[4]);
 	int seed = atoi(argv[5]);
+	float rvfrac = atof(argv[6]);
 	/*int erf_id=60;
 	int src=0;
 	int rup=0;
@@ -22,14 +23,16 @@ int main(int argc, char** argv) {
 
     set_memcached_server("localhost");
     char* rupture_file = malloc(sizeof(char)*256);
-    //sprintf(rupture_file, "/gpfs/alpine/proj-shared/geo112/CyberShake/ruptures/Ruptures_erf%d/%d/%d/%d_%d.txt", erf_id, src, rup, src, rup);
-    sprintf(rupture_file, "m6.73-0.10x0.10.gsf");
+    sprintf(rupture_file, "/gpfs/alpine/proj-shared/geo112/CyberShake/ruptures/Ruptures_erf%d/%d/%d/%d_%d.txt", erf_id, src, rup, src, rup);
+    //sprintf(rupture_file, "m6.73-0.10x0.10.gsf");
     char outfile[256];
     struct standrupformat srf;
 	struct rg_stats_t stats;
 	char params[2048];
 	sprintf(params, "seed=%d", seed);
-	sprintf(params, "%s read_erf=0 read_gsf=1 calc_shypo=0 shypo=6.00 dhypo=19.40", params);
+	sprintf(params, "%s rvfrac=%f", params, rvfrac);
+	sprintf(params, "%s use_unmodified_seed=1", params);
+	/*sprintf(params, "%s read_erf=0 read_gsf=1 calc_shypo=0 shypo=6.00 dhypo=19.40", params);
 	sprintf(params, "%s mag=6.73 nstk=200 ndip=270 ns=1 nh=1 rvfrac=0.811046", params);
 	sprintf(params, "%s kmodel=2 slip_sigma=0.75 circular_average=0", params);
 	sprintf(params, "%s modified_corners=0 shal_vrup_dep=6.500000 shal_vrup_deprange=1.500000", params);
@@ -40,8 +43,8 @@ int main(int argc, char** argv) {
 	sprintf(params, "%s flen_max=-1.000000 rupture_delay=0.000000 moment_fraction=-1.000000 srf_version=2.0 rake_sigma=15.0", params);
 	sprintf(params, "%s fdrup_time=1 deep_vrup=0.6 use_gaus=1 alpha_rough=0.01 lambda_min=0.08", params);
 	sprintf(params, "%s tsfac_coef=1.1 tsfac1_sigma=1.0 tsfac1_scor=0.8 rtime1_sigma=0.750000 rtime1_scor=0.5 tsfac_bzero=-0.1 tsfac_slope=-0.5", params);
-	sprintf(params, "%s velfile=/gpfs/alpine/proj-shared/geo112/CyberShake/software/bbp/bbp_data/indata/4277838/nr02-vs500.fk1d", params);
-	rupgen_genslip_with_params(rupture_file, rv, 0, &stats, &srf, RUPGEN_PROVIDE_HYPO, 0.1, params);
+	sprintf(params, "%s velfile=/gpfs/alpine/proj-shared/geo112/CyberShake/software/bbp/bbp_data/indata/4277838/nr02-vs500.fk1d", params);*/
+	rupgen_genslip_with_params(rupture_file, rv, 0, &stats, &srf, RUPGEN_UNIFORM_HYPO, 0.05, params);
 	sprintf(outfile, "%d_%d_%d_seed%d_uniform.srf", src, rup, rv, seed);
 	write_srf(&srf, outfile, 0);
     free_srf_ptrs(&srf);
