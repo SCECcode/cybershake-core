abb21982cb5dc66939a2088c5f665bd9f522ef45
Scott Callaghan <callag@login10.frontier.olcf.ornl.gov>
2024-05-21 17:29:11 -0400

    Fixes related to taper options and SFCVM.

--- UCVM/src/get_model_info_for_db.c
+++ UCVM/src/get_model_info_for_db.c
@@ -12,8 +12,13 @@ int ifless_taper = 0;
 double taper_depth = 700.0;
 int ely_init = 0;
 
+const char* UCVM_CONF = "/lustre/orion/geo156/proj-shared/CyberShake/software/UCVM/ucvm_22.7.0_withSFCVM/conf/ucvm.conf";
+
 void initialize_ucvm(char* model) {
-	if (ucvm_init("/gpfs/alpine/proj-shared/geo112/CyberShake/software/UCVM/ucvm-22.7.0/conf/ucvm.conf")!=UCVM_CODE_SUCCESS) {
+	if (ucvm_initialized) {
+		return;
+	}
+	if (ucvm_init(UCVM_CONF)!=UCVM_CODE_SUCCESS) {
                 fprintf(stderr, "Failed to init UCVM, aborting.");
                 exit(3);
         }
@@ -29,8 +34,6 @@ void initialize_ucvm(char* model) {
 			model_string = UCVM_MODEL_1D;
 		} else if (strcmp(model, "cvmsi")==0) {
 			model_string = UCVM_MODEL_CVMSI;
-			//Also activate the taper
-			ifless_taper = 1;
 		} else if (strcmp(model, "usgs")==0) {
 			model_string = UCVM_MODEL_CENCAL;
 		} else if (strcmp(model, "cvms5")==0) {
@@ -41,13 +44,15 @@ void initialize_ucvm(char* model) {
 			model_string = "cca";
 		} else if (strcmp(model, "bbp1d")==0) {
 			model_string = "bbp1d";
+		} else if (strcmp(model, "sfcvm")==0) {
+			model_string = "sfcvm";
 		} else {
 			printf("Model string %s didn't match any known models, aborting.", model_string);
 			exit(2);
 		}
 		printf("Adding model %s.\n", model_string);
                 if (ucvm_add_model(model_string)!=UCVM_CODE_SUCCESS) {
-                        fprintf(stderr, "Error retrieving UCVM model %s.", model_string);
+                        fprintf(stderr, "Error retrieving UCVM model %s.\n", model_string);
                         exit(3);
                 }
 	} else {
@@ -62,8 +67,6 @@ void initialize_ucvm(char* model) {
                 	        model_string = UCVM_MODEL_1D;
                 	} else if (strcmp(tok, "cvmsi")==0) {
                 	        model_string = UCVM_MODEL_CVMSI;
-							//Also activate the taper
-							ifless_taper = 1;
                 	} else if (strcmp(tok, "usgs")==0) {
                 	        model_string = UCVM_MODEL_CENCAL;
                 	} else if (strcmp(tok, "cvms5")==0) {
@@ -72,13 +75,17 @@ void initialize_ucvm(char* model) {
                 	        model_string = UCVM_MODEL_BBP1D;
                 	} else if (strcmp(tok, "cca")==0) {
                 	        model_string = "cca";
+					} else if (strcmp(tok, "sfcvm")==0) {
+							model_string = "sfcvm";
+					} else if (strcmp(tok, "bbp1d")==0) {
+							model_string = UCVM_MODEL_BBP1D;
                 	} else {
-                	        printf("Model string %s didn't match any known models, aborting.", model_string);
+                	        printf("Model string %s didn't match any known models, aborting.\n", tok);
                 	        exit(2);
                 	}
                 	printf("Adding model %s.\n", model_string);
                 	if (ucvm_add_model(model_string)!=UCVM_CODE_SUCCESS) {
-                	        fprintf(stderr, "Error retrieving UCVM model %s.", model_string);
+                	        fprintf(stderr, "Error retrieving UCVM model %s.\n", model_string);
                 	        exit(3);
                 	}
 			tok = strtok_r(NULL, ",", &save);
@@ -87,9 +94,34 @@ void initialize_ucvm(char* model) {
 	ucvm_initialized = 1;
 }
 
+int test_for_taper(float lon, float lat, char* taper_models) {
+	ucvm_point_t query_pt;
+	ucvm_data_t query_data;
+	query_pt.coord[0] = lon;
+	query_pt.coord[1] = lat;
+	query_pt.coord[2] = 0.0;
+	if (ucvm_query(1, &query_pt, &query_data)!=UCVM_CODE_SUCCESS) {
+		fprintf(stderr, "UCVM query failed.\n");
+        exit(-3);
+    }
+	char pt_label[64];
+	ucvm_model_label(query_data.crust.source, pt_label, 64);
+	char* tok = strtok(taper_models, ",");
+	char tmp[64];
+	while (tok!=NULL) {
+		strcpy(tmp, tok);
+		if (strcmp(tmp, pt_label)==0) {
+			//Yes, we will be needing the taper
+			return 1;
+		}
+		tok = strtok(NULL, ",");
+	}
+	return 0;
+}
+
 
 //Retrieves vs30 using Vs30 = 30 / sum( 1 / (Vs sampled from [0.5, 29.5] at 1 meter increments, for 30 values) )
-float ucvm_vs30(float lon, float lat) {
+float ucvm_vs30(float lon, float lat, int taper_flag) {
 		ucvm_point_t* query_pts = malloc(sizeof(ucvm_point_t)*30);
 		ucvm_data_t* query_data = malloc(sizeof(ucvm_data_t)*30);
 		double* vs_results = malloc(sizeof(double)*30);
@@ -109,7 +141,7 @@ float ucvm_vs30(float lon, float lat) {
         }
 		
 		//Include the taper, if needed
-	    if (ifless_taper!=0) {
+	    if (taper_flag!=0) {
 	        printf("Using ifless taper.\n");
     	    //If we're using the taper, we need to initialize Ely
     	    //Similar code to that in ucvm-single_mpi.c
@@ -161,7 +193,7 @@ float ucvm_vs30(float lon, float lat) {
 }
 
 //Determines second crossing (if it exists) of Z-values, with 10 m resolution, down to 50 km
-float ucvm_zvalue(float lon, float lat, float vs_value) {
+float ucvm_zvalue(float lon, float lat, float vs_value, int taper_flag) {
 	float resolution = 10.0;
 	float max_depth = 50000.0;
 	int num_pts = (int)(max_depth/resolution)+1;
@@ -184,7 +216,7 @@ float ucvm_zvalue(float lon, float lat, float vs_value) {
 	}
 
     //If we need to include the taper
-    if (ifless_taper!=0) {
+    if (taper_flag!=0) {
         printf("Using ifless taper.\n");
         //If we're using the taper, we need to initialize Ely
         //Similar code to that in ucvm-single_mpi.c
@@ -254,7 +286,7 @@ float ucvm_zvalue(float lon, float lat, float vs_value) {
 
 int main(int argc, char** argv) {
 	if (argc<5) {
-		printf("Usage: %s <lat> <lon> <comma-separated model string> <output file>\n", argv[0]);
+		printf("Usage: %s <lat> <lon> <comma-separated model string> <output file> [comma-separated taper string]\n", argv[0]);
 		exit(1);
 	}
 	float lat = atof(argv[1]);
@@ -262,10 +294,21 @@ int main(int argc, char** argv) {
 	char* model_string = argv[3];
 	char* output_file = argv[4];
 
+    initialize_ucvm(model_string);
+
+	char* taper_models = NULL;
+	int taper_flag = 0;
+	if (argc>5) {
+		taper_models = argv[5];
+		//Since all the queries are for a single point, determine if this point is using a model that requires the taper
+		taper_flag = test_for_taper(lon, lat, taper_models);
+		printf("Taper flag = %d.\n", taper_flag);
+	}
+
 	initialize_ucvm(model_string);
-	float vs30 = ucvm_vs30(lon, lat);
-	float z10 = ucvm_zvalue(lon, lat, 1000.0);
-	float z25 = ucvm_zvalue(lon, lat, 2500.0);
+	float vs30 = ucvm_vs30(lon, lat, taper_flag);
+	float z10 = ucvm_zvalue(lon, lat, 1000.0, taper_flag);
+	float z25 = ucvm_zvalue(lon, lat, 2500.0, taper_flag);
 
 	FILE* fp_out = fopen(output_file, "w");
 	fprintf(fp_out, "%.1f\n", vs30);
