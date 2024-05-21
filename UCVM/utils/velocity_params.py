a9575d68aca535b68c55906ef16f83ba352db342
Scott Callaghan <callag@login10.frontier.olcf.ornl.gov>
2024-05-21 17:30:00 -0400

    Added option for local sqlite database.

--- UCVM/utils/velocity_params.py
+++ UCVM/utils/velocity_params.py
@@ -6,6 +6,7 @@ import sys
 import os
 import pymysql
 import argparse
+import sqlite3
 
 full_path = os.path.abspath(sys.argv[0])
 path_add = os.path.dirname(os.path.dirname(os.path.dirname(full_path)))
@@ -21,8 +22,9 @@ parser.add_argument("-m", "--models", help="Comma-separated string of UCVM veloc
 parser.add_argument("-vmesh", "--velocity_mesh", help="Path to velocity mesh file")
 parser.add_argument("-fd", "--fdloc", help="Path to fdloc file with site coordinates")
 parser.add_argument("-go", "--gridout", help="Path to gridout file")
-parser.add_argument("-s", "--server", help="Database server to use")
+parser.add_argument("-s", "--server", help="Database server to use.  Default is moment.usc.edu.  Can specify sqlite database with sqlite://<file>.  ***Note that if LOCAL_DB is defined in cybershake.cfg file, that value will be used instead of what is given here.***")
 parser.add_argument("-r", "--run_id", help="Run ID")
+parser.add_argument("-tm", "--taper_models", help="Comma-separate string of models to apply taper to.", type=str)
 
 args = parser.parse_args()
 if args.latitude==None or args.longitude==None or args.models==None or args.velocity_mesh==None or args.gridout==None or args.fdloc==None or args.run_id==None:
@@ -32,14 +34,24 @@ if args.latitude==None or args.longitude==None or args.models==None or args.velo
 
 server = "moment.usc.edu"
 if args.server is not None:
-	server = args.server
+    server = args.server
+
+try:
+	local_db = config.getProperty("LOCAL_DB")
+	server = local_db
+except KeyError:
+	pass
+
+taper_models = ""
+if args.taper_models is not None:
+	taper_models = args.taper_models
 
 #Call get_model_info_for_db to get model Vs30, Z1.0, Z2.5
 model_output_file = "ucvm_data.txt"
-UCVM_HOME = "/gpfs/alpine/proj-shared/geo112/CyberShake/software/UCVM/ucvm-22.7.0"
-ld_lib_path="%s/lib/euclid3/lib:%s/lib/proj-5/lib:%s/model/cvmsi/lib:%s/model/cencal/lib:%s/model/cvms5/lib:%s/model/cca/lib:%s/model/cvms/lib" % (UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME)
-proj_lib_path="/gpfs/alpine/proj-shared/geo112/CyberShake/software/UCVM/ucvm-22.7.0/lib/proj-5/share/proj"
-cmd = "export PROJ_LIB=%s; export LD_LIBRARY_PATH=%s:$LD_LIBRARY_PATH; %s/UCVM/bin/get_model_info_for_db %f %f %s %s" % (proj_lib_path, ld_lib_path, config.getProperty("CS_PATH"), args.latitude, args.longitude, args.models, model_output_file)
+UCVM_HOME = "/lustre/orion/geo156/proj-shared/CyberShake/software/UCVM/ucvm_22.7.0_withSFCVM"
+ld_lib_path="%s/lib/euclid3/lib:%s/lib/proj/lib:%s/model/cvmsi/lib:%s/model/cencal/lib:%s/model/cvms5/lib:%s/model/cca/lib:%s/model/cvms/lib:%s/model/sfcvm/lib" % (UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME, UCVM_HOME)
+proj_lib_path = "/lustre/orion/geo156/proj-shared/CyberShake/software/UCVM/ucvm_22.7.0_withSFCVM/lib/proj/share/proj"
+cmd = "export PROJ_LIB=%s; export LD_LIBRARY_PATH=%s:$LD_LIBRARY_PATH; %s/UCVM/bin/get_model_info_for_db %f %f %s %s %s" % (proj_lib_path, ld_lib_path, config.getProperty("CS_PATH"), args.latitude, args.longitude, args.models, model_output_file, taper_models)
 print(cmd)
 os.system(cmd)
 
@@ -78,7 +90,11 @@ with open(db_file, "r") as fp_in:
     password = fp_in.readline().strip()
     fp_in.close()
 
-conn = pymysql.connect(host=server, db="CyberShake", user=username, passwd=password)
+if server.find("sqlite://")==0:
+	#Connect to local sqlite db
+	conn = sqlite3.connect(server[9:])
+else:
+	conn = pymysql.connect(host=server, db="CyberShake", user=username, passwd=password)
 
 cur = conn.cursor()
 update = "update CyberShake_Runs set Model_Vs30=%f, Mesh_Vsitop=%f, Z1_0=%f, Z2_5=%f where Run_ID=%d" % (model_vs30, mesh_vs, model_z10, model_z25, int(args.run_id))
