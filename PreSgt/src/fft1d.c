ec891e59149873b1d409b09820959b118835dc0c
Scott Callaghan <callag@login10.frontier.olcf.ornl.gov>
2024-05-21 17:16:40 -0400

    Fixed non-standard function prototypes.

--- PreSgt/src/fft1d.c
+++ PreSgt/src/fft1d.c
@@ -1,5 +1,10 @@
 #include <math.h>
 
+struct complex { float re; float im; };
+
+void cfft_r(struct complex* x,int n,int isign);
+void cfft(struct complex* x,int n,int isign);
+
 float sin_table[] =
    {
 	1.0000000e+00,	/* sin(pi/2) */
@@ -33,10 +38,7 @@ float sin_table[] =
  *
  * the routine does no normalization
  */
-struct complex { float re; float im; };
-cfft(x,n,isign)
-struct complex *x;
-int n,isign;
+void cfft(struct complex* x,int n,int isign)
    {
 	register struct complex *px, *qx, *rx;
 	struct complex *limit, *qlimit, dtemp;
@@ -232,9 +234,7 @@ int n, isign;
 	cfft_r(x,n/2,isign);
    }
 
-cfft_r(x,n,isign)
-struct complex *x;
-int n,isign;
+void cfft_r(struct complex* x,int n,int isign)
    {
 	register struct complex *px, *qx, *rx;
 	struct complex *limit, *qlimit, dtemp;
