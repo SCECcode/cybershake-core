8de2407f29ad47ad42062464ecf047225f5de733
Scott Callaghan <scottcal@usc.edu>
2024-08-27 14:55:42 -0700

    Changed filters so as not to get confused with vertical response and period-dependent durations.  Enabled RotD50-only functions.  Support for moment-carc.

--- CyberCommands/util/RotDFilenameFilter.java
+++ CyberCommands/util/RotDFilenameFilter.java
@@ -8,7 +8,7 @@ import javax.swing.filechooser.FileFilter;
 public class RotDFilenameFilter implements FilenameFilter {
 
 	public boolean accept(File dir, String name) {
-		if (name.endsWith(".rotd")) {
+		if (name.startsWith("RotD") && name.endsWith(".rotd")) {
 			return true;
 		}
 		return false;
