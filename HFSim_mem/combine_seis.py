0ea18bda47ea2cb1a1cfb638d44399c0a69a4838
Scott Callaghan <scottcal@login4.frontera.tacc.utexas.edu>
2024-09-24 15:44:19 -0500

    Converted to Python3

--- HFSim_mem/combine_seis.py
+++ HFSim_mem/combine_seis.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import os
 
 if len(sys.argv)<3:
-	print "Usage: %s <seis 0> <seis 1> ... <seis N> <output seis name>" % sys.argv[0]
+	print("Usage: %s <seis 0> <seis 1> ... <seis N> <output seis name>" % sys.argv[0])
 	sys.exit(1)
 
 seis_out = sys.argv[-1]
@@ -13,5 +13,5 @@ seis_in = sys.argv[1:-1]
 cmd = "cat %s > %s" % (' '.join(seis_in), seis_out)
 exitcode = os.system(cmd)
 if exitcode!=0:
-	print "Command '%s' failed." % (cmd)
+	print("Command '%s' failed." % (cmd))
 sys.exit(exitcode)
