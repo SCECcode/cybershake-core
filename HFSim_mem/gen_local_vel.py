2a1d6233ecdcef0b5cf3fc06651a06b228184160
Scott Callaghan <scottcal@login4.frontera.tacc.utexas.edu>
2024-09-24 15:44:27 -0500

    Fixed spacing issues.

--- HFSim_mem/gen_local_vel.py
+++ HFSim_mem/gen_local_vel.py
@@ -19,21 +19,21 @@ vm_fp.close()
 #vel_out_fp = open(vel_out, "w")
 vel_out_fp = open(out_filename, "w")
 for line in vel_in_data:
-	i += 1
-        if line.startswith("#") or line.startswith("%"):
-                continue
-        pieces = line.split()
-        if len(pieces)>=4:
-                th=float(pieces[0])
-                vp=float(pieces[1])
-                vs=float(pieces[2])
-                dn=float(pieces[3])
-                qs=c0+c1*vs
-                if i==len(vel_in_data):
-                        th=0.0
-                vel_out_fp.write("%8.4f %8.4f %8.4f %8.4f %8.2f %8.2f\n" % (th, vp, vs, dn, qs, qs))
-        else:
-                vel_out_fp.write(line)
+    i += 1
+    if line.startswith("#") or line.startswith("%"):
+            continue
+    pieces = line.split()
+    if len(pieces)>=4:
+        th=float(pieces[0])
+        vp=float(pieces[1])
+        vs=float(pieces[2])
+        dn=float(pieces[3])
+        qs=c0+c1*vs
+        if i==len(vel_in_data):
+            th=0.0
+        vel_out_fp.write("%8.4f %8.4f %8.4f %8.4f %8.2f %8.2f\n" % (th, vp, vs, dn, qs, qs))
+    else:
+        vel_out_fp.write(line)
 vel_out_fp.flush()
 vel_out_fp.close()
 sys.exit(0)
